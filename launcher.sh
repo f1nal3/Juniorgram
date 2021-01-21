7f1e3d0cef58bb227c5b649dd099e1fa708b518c
Aliaksandr Kasianchuk-glaush <Aliaksandr_Kasianchuk@epam.com>
2021-01-21 19:02:15 +0000

    Merged PR 4: PR 3: Added Conan integration and install targets rules.
    PR 3: Added Conan integration and install targets rules.
    
    Related work items: #12

--- launcher.sh
+++ launcher.sh
@@ -3,3 +3,14 @@ mkdir build
 cd build
 
 cmake ..
+
+#To prevent the console from closing
+read -n1 -s -r -p $'Press space to close console window...\n' key
+
+if [ "$key" = ' ' ]; then
+    #Space pressed, do something
+    #Echo [$key] is empty when SPACE is pressed # uncomment to trace
+else
+    #Anything else pressed, do whatever else.
+    #echo [$key] not empty
+fi
