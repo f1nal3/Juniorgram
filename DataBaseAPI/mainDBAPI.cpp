d51ae2ed1750959c71642ed04eb0bab5aca65c4c
Aliaksandr Staravoitau <aliaksandr_staravoitau1@epam.com>
2021-02-14 14:27:04 +0300

    Added simple postgreSQL project

--- DataBaseAPI/mainDBAPI.cpp
+++ DataBaseAPI/mainDBAPI.cpp
@@ -7,9 +7,23 @@
 suppressWarning(4099, -winit-self)
 int main()
 {   
-    pqxx::result res;
+    try
+    {
+        pqxx::connection c("dbname=test user=postgres password=123 hostaddr=127.0.0.1 port=5432");
 
-    std::cin.get();
+        pqxx::work w(c);
+
+        pqxx::row r = w.exec1("SELECT 1");
+
+        w.commit();
+
+        std::cout << r[0].as<int>() << std::endl;
+    }
+    catch (std::exception const &e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
     return 0;
 }
 restoreWarning
