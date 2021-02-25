bd112e76a5e46559fcace3fb672348ede8dd2e7d
Aliaksandr Staravoitau <47822816+vDrWool@users.noreply.github.com>
2021-02-25 17:56:55 +0300

    PostgreSQL v0.1 (#52)
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Minors
    
    * Version 0.0.1 of API
    
    * Api version 0.0.2
    
    * Some fixes
    
    * Tested some methods
    
    * Fixed some bugs
    
    * Shared_ptr fix
    
    * Shared_ptr fix 2.0
    
    * Added simple example (mainDBAPI)
    
    * Fixes
    
    * Fixes 2.0
    
    * Fixes 3.0
    
    Co-authored-by: Valentine Timoshik <valentine.timoshik@gmail.com>
    Co-authored-by: vDrWool <aliaksandr_staravoitau1@epam.com>
    Co-authored-by: Svidunovich <q77qw2000@gmail.com>

--- DataBaseAPI/Headers/DataBasePostgreSQL.hpp
+++ DataBaseAPI/Headers/DataBasePostgreSQL.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <mutex>
+#include <memory>
+#include <optional>
 
 #include <pqxx/pqxx>
 
@@ -7,5 +9,273 @@
 
 namespace DBPostgre
 {
-    
+    // Thread safe singleton.
+    class PostgreSQL
+    {
+    private:
+
+        static std::mutex sm_static_mutex;
+        static std::shared_ptr<PostgreSQL> spm_instance;
+
+        std::mutex m_query_mutex;
+        pqxx::connection m_connection;
+
+    protected:
+
+        PostgreSQL(const std::string_view& options)
+            : m_connection{ pqxx::zview(options) } {}
+
+    public:
+
+        PostgreSQL(const PostgreSQL& other) = delete;
+        PostgreSQL& operator = (const PostgreSQL& other) = delete;
+
+        PostgreSQL(PostgreSQL&& other) = delete;
+        PostgreSQL& operator = (PostgreSQL&& other) = delete;
+
+    public:
+        
+        static std::shared_ptr<PostgreSQL> getPostgre(const std::string_view& options = "")
+        {
+            std::lock_guard<std::mutex> lock(sm_static_mutex);
+
+            if (spm_instance == nullptr)
+            {
+                if (options.empty())
+                {  
+                    throw pqxx::broken_connection(
+                        "Instance didn't create because options are incorrect!");
+                }
+
+                spm_instance = std::shared_ptr<PostgreSQL>(new PostgreSQL(options));
+            }
+
+            return spm_instance;
+        }
+
+        pqxx::connection& getConnection()
+        {
+            return this->m_connection;
+        }
+
+        bool isConnected() const
+        {
+            return this->m_connection.is_open();
+        }
+
+        std::optional<pqxx::result> query(const std::string_view& query)
+        {
+            std::lock_guard<std::mutex> lock(this->m_query_mutex);
+
+            pqxx::work work{ this->m_connection };
+
+            auto res = work.exec(query);
+
+            work.commit();
+
+            if (!res.empty())
+                return std::optional(res);
+
+            return std::nullopt;
+        }
+
+    };
+
+    std::mutex PostgreSQL::sm_static_mutex{};
+    std::shared_ptr<PostgreSQL> PostgreSQL::spm_instance{};
+
+// ============================================================================================
+
+    class PostgreTableOperations : public DataBases::IDataBaseTableOperations
+    {
+    private:
+
+        std::shared_ptr<PostgreSQL> m_dbInstanse;
+
+    public:
+
+        PostgreTableOperations(const std::shared_ptr<PostgreSQL> postgre)
+            : m_dbInstanse{ postgre } {}
+
+
+        PostgreTableOperations(const PostgreTableOperations& other) = delete;
+        PostgreTableOperations& operator = (const PostgreTableOperations& other) = delete;
+
+        PostgreTableOperations(PostgreTableOperations&& other) = delete;
+        PostgreTableOperations& operator = (PostgreTableOperations&& other) = delete;
+
+    public:
+
+        // You need to write 'tableFields' like:
+        //  columnName1 type,
+        //  columaName2 type,
+        //  N-----------------N
+        //  columnNameN type
+        void createTable(const std::string_view& tableName,
+                         const std::string_view& tableFields) const override
+        {
+            pqxx::work work{ this->m_dbInstanse->getConnection() };
+
+            work.exec0("CREATE TABLE IF NOT EXISTS " + work.esc(tableName) + 
+                         "( " + work.esc(tableFields) + " );");
+
+            work.commit();
+        }
+
+        void deleteTable(const std::string_view& tableName) const override
+        {
+            pqxx::work work{ this->m_dbInstanse->getConnection() };
+
+            work.exec0("DROP TABLE IF EXISTS " + work.esc(tableName) + ';');
+
+            work.commit();
+        }
+
+    };
+
+    class PostgreFieldOperations : public DataBases::IDataBaseFieldOperations
+    {
+    private:
+
+        std::shared_ptr<PostgreSQL> m_dbInstanse;
+
+    public:
+
+        PostgreFieldOperations(const std::shared_ptr<PostgreSQL> postgre)
+            : m_dbInstanse{ postgre } {} 
+
+
+        PostgreFieldOperations(const PostgreFieldOperations& other) = delete;
+        PostgreFieldOperations& operator = (const PostgreFieldOperations& other) = delete;
+
+        PostgreFieldOperations(PostgreFieldOperations&& other) = delete;
+        PostgreFieldOperations& operator = (PostgreFieldOperations&& other) = delete;
+
+    public:
+
+        // additional => "WHERE", "ORDER BY", ...
+        // If you don't need 'additional', but need 'numberOfTheColumns' use = "".
+        // numberOfTheColumns = "columnName1, columnName2, ..."
+        [[nodiscard]] pqxx::result select(const std::string& tableName,
+                                          const std::string& columnsNames,
+                                          const std::string& additional = {},
+                                          const pqxx::result::size_type numberOfTheRows = -1) const 
+        { 
+            pqxx::nontransaction work{ this->m_dbInstanse->getConnection() };
+
+            const std::string query = "SELECT " + work.esc(columnsNames) + " FROM " +
+                                     work.esc(tableName) + ' ' + work.esc(additional) + ';';
+            if (numberOfTheRows == -1) 
+                return work.exec(query);
+
+            return work.exec_n(numberOfTheRows, query);
+        }
+
+        // INSERT INTO 'tableName'(columnsNames) VALUES (...)
+        // columnsNames = 'columnName1, columnName2, ...'
+        void insert(const std::string_view& tableName,
+                    const std::string& data,
+                    const std::string_view& columnsNames = {},
+                    const std::string_view& additional = {}) const override
+        {
+            pqxx::work work{ this->m_dbInstanse->getConnection() };
+
+            std::string columns{};
+            if (!columnsNames.empty())
+			   	columns = '(' + work.esc(columnsNames) + ')';
+
+            work.exec0("INSERT INTO " + work.esc(tableName) + columns + " VALUES (" +
+                         data + ") " + work.esc(additional) + ';');
+
+            work.commit();
+        }
+
+        void update(const std::string_view& tableName,
+                    const std::string_view& columnsNamesAndNewData,
+                    const std::string_view& additional = {}) const override
+        {
+            pqxx::work work{ this->m_dbInstanse->getConnection() };
+
+            work.exec0("UPDATE " + work.esc(tableName) + " SET " +
+                work.esc(columnsNamesAndNewData) + work.esc(additional) + ';');
+
+            work.commit();
+        }
+
+        // !!! IF YOU DON'T USE 'additional' YOU MAY CLEAR ALL TABLE !!!
+        void del(const std::string_view& tableName,
+                 const std::string_view& additional = {}) const override
+        {
+            pqxx::work work{this->m_dbInstanse->getConnection()};
+
+            work.exec0("DELETE FROM " + work.esc(tableName) + work.esc(additional) + ';');
+
+            work.commit();
+        }
+
+        // SELECT * FROM 'tableName' WHERE 'column' = 'data'
+        bool isExist(const std::string_view& tableName,
+                     const std::string_view& column,
+                     const std::string& data) const override
+        {
+            pqxx::nontransaction work{this->m_dbInstanse->getConnection()};
+
+            try
+            {
+                pqxx::row res = work.exec1("SELECT * FROM " +
+                    work.esc(tableName) + " WHERE " + work.esc(column) + 
+                    " = " + work.esc(data) + ';');
+
+                if (!res.empty())
+				   	return true;
+            }
+            catch (...)
+            {
+                return false; 
+            }
+
+            return false;   
+        }
+
+    };
+
+    class PostgreColumnOperations : public DataBases::IDataBaseColumnOperations
+    {
+    private:
+
+        std::shared_ptr<PostgreSQL> m_dbInstanse;
+
+    public:
+
+        PostgreColumnOperations(const std::shared_ptr<PostgreSQL> postgre)
+            : m_dbInstanse{ postgre } {}
+        
+
+    public:
+
+        void addNewColumn(const std::string_view& tableName,
+                          const std::string_view& newColumn,
+                          const std::string_view& columnType) const override
+        {
+            pqxx::work work{ this->m_dbInstanse->getConnection() };
+
+            work.exec0("ALTER TABLE " + work.esc(tableName) + " ADD COLUMN " + 
+                work.esc(newColumn) + ' ' + work.esc(columnType) + ';');
+
+            work.commit();
+        }
+
+        void deleteColumn(const std::string_view& tableName,
+                          const std::string_view& columnName) const override
+        {
+            pqxx::work work{this->m_dbInstanse->getConnection()};
+
+            work.exec0("ALTER TABLE " + work.esc(tableName) + " DROP COLUMN " + 
+                work.esc(columnName) + ';');
+            
+            work.commit();
+        }
+
+    };
 }
+
--- DataBaseAPI/mainDBAPI.cpp
+++ DataBaseAPI/mainDBAPI.cpp
@@ -2,28 +2,71 @@
 
 #include "../common/WarningSuppressing.h"
 
-#include <pqxx/pqxx>
+#include "Headers/DataBasePostgreSQL.hpp"
 
 suppressWarning(4099, -Winit-self)
 int main()
 {   
     try
     {
-        pqxx::connection c("dbname=test user=postgres password=123 hostaddr=127.0.0.1 port=5432");
 
-        pqxx::work w(c);
+        using namespace std::literals::string_view_literals;
 
-        pqxx::row r = w.exec1("SELECT 1");
+        auto psql = DBPostgre::PostgreSQL::getPostgre(
+            "dbname=postgres user=postgres password=Asw789654 hostaddr=127.0.0.1 port=5432");
+        psql->getConnection().set_client_encoding("WIN866");
 
-        w.commit();
+        const std::string_view columns
+        {   
+            "id integer, "
+            "name varchar(32), "
+            "age integer "
+        };
 
-        std::cout << r[0].as<int>() << std::endl;
+        DBPostgre::PostgreTableOperations tbl{ psql };
+        tbl.deleteTable("test"sv);
+        tbl.createTable("test"sv, columns);
+
+        DBPostgre::PostgreColumnOperations clmn{ psql };
+        clmn.addNewColumn("test"sv, "password"sv, "varchar(32)");
+
+        DBPostgre::PostgreFieldOperations fld{ psql };
+        fld.insert("test"sv, "0, 'a', 25, 'a52b'");
+        fld.insert("test"sv, "1, 'b', 31, 'b2c1'");
+        fld.insert("test"sv, "2, 'c', 21, '24cb'");
+        fld.insert("test"sv, "3, 'd', 33, '112a'");
+        fld.insert("test"sv, "4, 'e', 30, '2421'");
+
+        auto res = fld.select("test", "*");
+
+        //suppressWarning(4239, -Winit-self)
+        for (auto/*&*/ row : res)
+        {
+            std::cout << row.at("id").as<int>() << ' ';
+            std::cout << row.at("name").as<std::string>() << ' ';
+            std::cout << row.at("age").as<int>() << ' ';
+            std::cout << row.at("password").as<std::string>() << '\n';
+        }
+        //restoreWarning
+
     }
-    catch (std::exception const &e)
+    catch (const pqxx::sql_error& e)
     {
         std::cerr << e.what() << '\n';
+        std::cerr << e.query() << '\n';
+        std::cerr << e.sqlstate() << '\n';
+        std::cin.get();
         return 1;
     }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        std::cin.get();
+        return 1;
+    }
+    
+    std::cin.get();
     return 0;
 }
 restoreWarning
+
