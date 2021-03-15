d24009c36dd32a4e43fc26e7e8b9b84a1b1f8aa0
Danil Fisiuk <61632356+fisyamaloy@users.noreply.github.com>
2021-03-15 19:48:50 +0300

    fix Argument Parser + create KeysValidator (#78)
    * fix Argument Parser + create KeysValidator
    
    * Fix fail build. (#79)
    
    * Fix build fail.
    
    * Replace CONAN_PKG::Qt to Qt5::<Package>.
    
    * Check folder structure.
    
    * Return ctest cmd.
    
    * Added tests for postgre. (#69)
    
    * Added tests for postgre
    
    * Fix
    
    * CMake n include files fixes
    
    * Change the password to connect to db. Delete the commnet in IDB.hpp. And try to change test conflict.
    
    * Fix cmake conflict.
    
    Co-authored-by: Valiantsin Tsimoshyk <Valiantsin_Tsimoshyk@epam.com>
    
    * fix
    
    * fix Argument Parser + create KeysValidator
    
    * fix
    
    * some fix
    
    * ArumentParserTests
    
    Co-authored-by: fisyamaloy <fisyamaloy@users.noreply.github.com>
    Co-authored-by: glaush <56369021+glaush@users.noreply.github.com>
    Co-authored-by: Aliaksandr Staravoitau <47822816+vDrWool@users.noreply.github.com>
    Co-authored-by: Valiantsin Tsimoshyk <Valiantsin_Tsimoshyk@epam.com>

---
+++ Server/Server.Test/ArgumentParserTests.cpp
@@ -0,0 +1,147 @@
+#include <catch2/catch.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../Server.Exec/ArgumentParser.h"
+
+TEST_CASE("Constructor argument parser")
+{
+    SECTION("Check a job with KeysValidator")
+    {
+        KeysValidator validator({"-r", "-k"}, {"-k"});
+        std::vector<const char*> args = {"path_to_project", "-k", "-r", "55000", "-d"};
+        CHECK_NOTHROW(ArgumentParser(static_cast<int>(args.size()), args.data(), validator));
+    }
+
+    SECTION("Only 1 input parameter throws an exception")
+    {
+        std::vector<const char*> args = {"path_to_project"};
+        CHECK_THROWS_AS(ArgumentParser(static_cast<int>(args.size()), args.data()), std::exception);
+    }
+
+    SECTION("Even number of keys without values throws an exception")
+    {
+        std::vector<const char*> args = {"path_to_project", "-p"};
+        CHECK_THROWS_AS(ArgumentParser(static_cast<int>(args.size()), args.data()), std::exception);
+    }
+
+    SECTION("Odd number of keys without values throws an exception")
+    {
+        std::vector<const char*> args = {"path_to_project", "-d", "131"};
+        CHECK_THROWS_AS(ArgumentParser(static_cast<int>(args.size()), args.data()), std::exception);
+    }
+
+    SECTION("Unvalid arguments keys throw an exeption")
+    {
+        {
+            std::vector<const char*> args = {"path_to_project", "any_key", "641", "-d"};
+            CHECK_THROWS_AS(ArgumentParser(static_cast<int>(args.size()), args.data()),
+                            std::exception);
+        }
+
+        {
+            std::vector<const char*> args = {"path_to_project", "-p", "27015", "any_key", "650000"};
+            CHECK_THROWS_AS(ArgumentParser(static_cast<int>(args.size()), args.data()),
+                            std::exception);
+        }
+    }
+
+    SECTION("Duplicated keys in arguments throw an exception")
+    {
+        {
+            std::vector<const char*> args = {"path_to_project", "-p", "27015", "-p", "6000"};
+            CHECK_THROWS_AS(ArgumentParser(static_cast<int>(args.size()), args.data()),
+                            std::exception);
+        }
+
+        {
+            std::vector<const char*> args = {"path_to_project", "-d", "-d", "-d"};
+            CHECK_THROWS_AS(ArgumentParser(static_cast<int>(args.size()), args.data()),
+                            std::exception);
+        }
+    }
+
+    SECTION("Coexistence realDB and fileDB keys in arguments throws an exception")
+    {
+        std::vector<const char*> args = {"path_to_project", "-p",
+                                         "27015"
+                                         "-d"};
+        CHECK_THROWS_AS(ArgumentParser(static_cast<int>(args.size()), args.data()), std::exception);
+    }
+
+    SECTION("Not existing realDB and fileDB key in arguments throws an exception")
+    {
+        std::vector<const char*> args = {"path_to_project", "-k", "-r"};
+        CHECK_THROWS_AS(ArgumentParser(static_cast<int>(args.size()), args.data(),
+                                       KeysValidator({"-k", "-r"}, {})),
+                        std::exception);
+    }
+}
+
+TEST_CASE("getPort()")
+{
+    SECTION("realDB key")
+    {
+        SECTION("Too big port value throws an exception")
+        {
+            std::vector<const char*> args = {"path_to_project", "-p", "99000"};
+            ArgumentParser ap(static_cast<int>(args.size()), args.data());
+            CHECK_THROWS_AS(ap.getPort(), std::exception);
+        }
+
+        SECTION("Too small port value throws an exception")
+        {
+            std::vector<const char*> args = {"path_to_project", "-p", "-2"};
+            ArgumentParser ap(static_cast<int>(args.size()), args.data());
+            CHECK_THROWS_AS(ap.getPort(), std::exception);
+        }
+
+        SECTION("getPort() == 27015")
+        {
+            {
+                std::vector<const char*> args = {"path_to_project", "-p", "27015"};
+                REQUIRE_NOTHROW(ArgumentParser(static_cast<int>(args.size()), args.data()));
+                ArgumentParser ap(static_cast<int>(args.size()), args.data());
+                REQUIRE(ap.getPort() == 27015);
+            }
+
+            {
+                std::vector<const char*> args = {"path_to_project", "   -p    ", "   27015   "};
+                REQUIRE_NOTHROW(ArgumentParser(static_cast<int>(args.size()), args.data()));
+                ArgumentParser ap(static_cast<int>(args.size()), args.data());
+                REQUIRE(ap.getPort() == 27015);
+            }
+        }
+    }
+
+    SECTION("fileDB key")
+    {
+        SECTION("getPort() throws an exception")
+        {
+            std::vector<const char*> args = {"path_to_project", "-d"};
+            REQUIRE_NOTHROW(ArgumentParser(static_cast<int>(args.size()), args.data()));
+            ArgumentParser ap(static_cast<int>(args.size()), args.data());
+            REQUIRE_THROWS_AS(ap.getPort(), std::exception);
+        }
+    }
+}
+
+TEST_CASE("isRealDB()")
+{
+    SECTION("with realDB key isRealDB() == true")
+    {
+        std::vector<const char*> args = {"path_to_project", "-p", "27015"};
+        REQUIRE_NOTHROW(ArgumentParser(static_cast<int>(args.size()), args.data()));
+        ArgumentParser ap(static_cast<int>(args.size()), args.data());
+        REQUIRE(ap.isRealDB() == true);
+    }
+
+    SECTION("with fileDB key isRealDB() == false")
+    {
+        std::vector<const char*> args = {"path_to_project", "-d"};
+        REQUIRE_NOTHROW(ArgumentParser(static_cast<int>(args.size()), args.data()));
+        ArgumentParser ap(static_cast<int>(args.size()), args.data());
+        REQUIRE(ap.isRealDB() == false);
+    }
+}
