659aeb4e468db2c9a4b10e591994ab34eb955f9e
Danil Fisiuk <61632356+fisyamaloy@users.noreply.github.com>
2021-03-15 19:48:50 +0300

    fix Argument Parser + create KeysValidator (#78)
    * fix Argument Parser + create KeysValidator
    
    * Fix fail build. (#79)
    
    * Fix build fail.
    
    * Replace CONAN_PKG::Qt to Qt5::<Package>.
    
    * Check folder structure.
    
    * Return ctest cmd.
    
    * Added tests for postgre. (#69)
    
    * Added tests for postgre
    
    * Fix
    
    * CMake n include files fixes
    
    * Change the password to connect to db. Delete the commnet in IDB.hpp. And try to change test conflict.
    
    * Fix cmake conflict.
    
    Co-authored-by: Valiantsin Tsimoshyk <Valiantsin_Tsimoshyk@epam.com>
    
    * fix
    
    * fix Argument Parser + create KeysValidator
    
    * fix
    
    * some fix
    
    * ArumentParserTests
    
    Co-authored-by: fisyamaloy <fisyamaloy@users.noreply.github.com>
    Co-authored-by: glaush <56369021+glaush@users.noreply.github.com>
    Co-authored-by: Aliaksandr Staravoitau <47822816+vDrWool@users.noreply.github.com>
    Co-authored-by: Valiantsin Tsimoshyk <Valiantsin_Tsimoshyk@epam.com>

---
+++ Server/Server.Exec/KeysValidator.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <algorithm>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+class KeysValidator
+{
+    using initList = std::initializer_list<std::string>;
+private:
+    struct Keys
+    {
+        const std::string realDB = "-p";
+        const std::string fileDB = "-d";
+    };
+
+private:
+    std::vector<std::string> validKeys;
+    std::vector<std::string> keysWithoutValues;
+
+public:
+    const Keys keys;
+
+    KeysValidator() noexcept
+    {
+        validKeys.emplace_back(keys.realDB);
+        validKeys.emplace_back(keys.fileDB);
+
+        keysWithoutValues.emplace_back(keys.fileDB);
+    }
+
+    KeysValidator(const initList& validkeysList, const initList& keysWithoutValuesList) noexcept
+        : KeysValidator()
+    {
+        for (auto&& key : validkeysList)
+        {
+            validKeys.emplace_back(key);
+        }
+
+        for (auto&& key : keysWithoutValuesList)
+        {
+            keysWithoutValues.emplace_back(key);
+        }
+    }
+
+    bool doKeyNeedValue(const std::string& incomingKey) const noexcept
+    {
+        return std::find(keysWithoutValues.begin(), keysWithoutValues.end(), incomingKey) ==
+               keysWithoutValues.end();
+    }
+
+    bool isKeyValid(const std::string& incomingKey) const noexcept
+    {
+        return std::find(validKeys.begin(), validKeys.end(), incomingKey) != validKeys.end();
+    }
+
+    KeysValidator& operator=(const KeysValidator& validator)
+    {
+        this->validKeys = validator.validKeys;
+
+        this->keysWithoutValues = validator.keysWithoutValues;
+
+        return *this;
+    }
+};
