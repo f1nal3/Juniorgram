a5649b9d052650c9b3b7cda05ed2016a0ab72277
Svidunovich <47819364+Svidunovich@users.noreply.github.com>
2021-03-15 20:48:33 +0300

    Add login and registration windows (#66)
    * Add login and registration windows
    
    * Remove .ui files & add login/registration widgets

---
+++ Client/Client.Qt/login.cpp
@@ -0,0 +1,42 @@
+#include "login.h"
+#include "registration.h"
+#include "mainwindow.h"
+
+Login::Login(QWidget *parent) : QWidget(parent)
+{
+    passwordLineEdit = std::make_unique<QLineEdit>();
+    usernameLineEdit = std::make_unique<QLineEdit>();
+    usernameLabel = std::make_unique<QLabel>("&Username:");
+    passwordLabel = std::make_unique<QLabel>("&Password:");
+    usernameLabel->setBuddy(&*usernameLineEdit);
+    passwordLabel->setBuddy(&*passwordLineEdit);
+
+    buttonSignin = std::make_unique<QPushButton>("&Signin");
+    buttonRegistration = std::make_unique<QPushButton>("&Registration");
+
+    QObject::connect(&*buttonSignin, SIGNAL(clicked()), this, SLOT(hide()));
+    QObject::connect(&*buttonSignin, SIGNAL(clicked()), this, SLOT(displayMainWindow()));
+    QObject::connect(&*buttonRegistration, SIGNAL(clicked()), this, SLOT(hide()));
+    QObject::connect(&*buttonRegistration, SIGNAL(clicked()), this, SLOT(displayRegistrationWindow()));
+
+    gridLayout = std::make_unique<QGridLayout>();
+    gridLayout->addWidget(&*usernameLabel, 0, 0);
+    gridLayout->addWidget(&*usernameLineEdit, 0, 1);
+    gridLayout->addWidget(&*passwordLabel, 1, 0);
+    gridLayout->addWidget(&*passwordLineEdit, 1, 1);
+    gridLayout->addWidget(&*buttonSignin, 2, 0);
+    gridLayout->addWidget(&*buttonRegistration, 3, 0);
+    this->setLayout(&*gridLayout);
+}
+
+void Login::displayRegistrationWindow()
+{
+    registrationWindow = std::make_unique<Registration>();
+    registrationWindow->show();
+}
+
+void Login::displayMainWindow()
+{
+    mainWindow = std::make_unique<MainWindow>();
+    mainWindow->show();
+}
