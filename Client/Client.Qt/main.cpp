d7b0e68f245650fcc08f75d9f8610340669c3268
Svidunovich <47819364+Svidunovich@users.noreply.github.com>
2021-03-15 20:48:33 +0300

    Add login and registration windows (#66)
    * Add login and registration windows
    
    * Remove .ui files & add login/registration widgets

--- Client/Client.Qt/main.cpp
+++ Client/Client.Qt/main.cpp
@@ -9,13 +9,13 @@ int main(int argc, char* argv[])
     w.show();
     w.windowHandle()->handle();
 #if _WIN32
-    HWND handle = reinterpret_cast<HWND>(w.winId());
-    LONG style = ::GetWindowLong(handle,GWL_STYLE);
-    ::SetWindowLong(handle,GWL_STYLE,style|WS_CAPTION|WS_MAXIMIZEBOX|WS_THICKFRAME);
-    ::SetWindowPos(handle,nullptr,0,0,0,0,SWP_FRAMECHANGED|SWP_NOMOVE|SWP_NOSIZE);
-    const MARGINS shadow {9,9,9,9};
-    ::DwmExtendFrameIntoClientArea(handle,&shadow);
-    w.showNormal();
+    HWND handle = reinterpret_cast<HWND>(w->winId());
+    LONG style  = ::GetWindowLong(handle, GWL_STYLE);
+    ::SetWindowLong(handle, GWL_STYLE, style | WS_CAPTION | WS_MAXIMIZEBOX | WS_THICKFRAME);
+    ::SetWindowPos(handle, nullptr, 0, 0, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE);
+    const MARGINS shadow{9, 9, 9, 9};
+    //::DwmExtendFrameIntoClientArea(handle,&shadow);
+    // w.showNormal();
 #endif
 
     return app.exec();
