d3a126379b98d9834e472fe32febf758f8900c42
Stanislav <stanislav.daschkevitc@yandex.by>
2021-03-15 18:27:42 +0300

    Some crashes

--- Client/Client.Qt/main.cpp
+++ Client/Client.Qt/main.cpp
@@ -1,20 +1,21 @@
+#include <QApplication>
 #include "mainwidget.h"
 #include "pch.h"
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-    QFontDatabase::addApplicationFont(":fonts/NotoSans-Regular.ttf");
-    auto w = std::make_unique<MainWidget>();
-    w->show();
+    MainWidget w;
+    w.show();
+    w.windowHandle()->handle();
 #if _WIN32
-    HWND handle = reinterpret_cast<HWND>(w->winId());
-    LONG style  = ::GetWindowLong(handle, GWL_STYLE);
-    ::SetWindowLong(handle, GWL_STYLE, style | WS_CAPTION | WS_MAXIMIZEBOX | WS_THICKFRAME);
-    ::SetWindowPos(handle, nullptr, 0, 0, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE);
-    const MARGINS shadow{9, 9, 9, 9};
+    HWND handle = reinterpret_cast<HWND>(w.winId());
+    LONG style = ::GetWindowLong(handle,GWL_STYLE);
+    ::SetWindowLong(handle,GWL_STYLE,style|WS_CAPTION|WS_MAXIMIZEBOX|WS_THICKFRAME);
+    ::SetWindowPos(handle,nullptr,0,0,0,0,SWP_FRAMECHANGED|SWP_NOMOVE|SWP_NOSIZE);
+    const MARGINS shadow {9,9,9,9};
     ::DwmExtendFrameIntoClientArea(handle,&shadow);
-    // w.showNormal();
+    w.showNormal();
 #endif
 
     return app.exec();
