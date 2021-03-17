ecd4b1e19978939ed9dda095df828b99bec5aea5
Valentine Timoshik <valentine.timoshik@gmail.com>
2021-03-17 22:51:35 +0300

    Moved serialize tests and created exception test

---
+++ Utility/Utility.Test/ExceptionTest.cpp
@@ -0,0 +1,15 @@
+#include <catch2/catch.hpp>
+
+#include "Utility/Exception.hpp"
+
+using Catch::Contains;
+
+void ExceptionThrowerMock()
+{
+    throw Utility::NotImplementedException("Not implemented", __FILE__, __LINE__);
+}
+
+TEST_CASE("Test NotImplementedException format", "[Utility.Exception]")
+{
+    REQUIRE_THROWS_WITH(ExceptionThrowerMock(), Contains("Not implemented") && Contains("ExceptionTest.cpp"));
+}
