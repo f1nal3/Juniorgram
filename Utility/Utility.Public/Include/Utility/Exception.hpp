6f34111fc9ed252dbfce42780d84cc6fd0058a67
Valentine Timoshik <valentine.timoshik@gmail.com>
2021-03-18 18:17:30 +0300

    IRepository WIP (#83)
    * IRepository WIP
    
    * Added namespace, PostgreRepository draft
    
    * Not implemented exception
    
    * PostgreRepository WIP
    
    * Moved serialize tests and created exception test
    
    * Review fixes

---
+++ Utility/Utility.Public/Include/Utility/Exception.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <stdexcept>
+#include <sstream>
+#include <string>
+
+inline std::string formatExceptionMessage(const std::string& msg, const std::string& file, std::uint8_t line)
+{
+    std::stringstream ss;
+
+    ss << msg << "\n in file: " << file << "\n on the line: " << line << std::endl;
+
+    return ss.str();
+}
+namespace Utility
+{
+    class NotImplementedException : public std::exception
+    {
+        public:
+        explicit NotImplementedException(const char* msg, const char* file, std::uint8_t line)
+            : _msg(formatExceptionMessage(msg, file, line))
+        {}
+
+        explicit NotImplementedException(const std::string& msg, const char* file, std::uint8_t line)
+            : _msg(formatExceptionMessage(msg, file, line))
+        {}
+
+        virtual const char* what() const noexcept
+        {
+            return _msg.c_str();
+        }
+        virtual ~NotImplementedException() = default;
+        
+        protected:
+        std::string _msg;
+        std::string _file;
+        std::uint8_t _line;
+    };
+}
