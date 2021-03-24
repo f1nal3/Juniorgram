bb65f17e797f31195c6566ddd7a7b1bcd4f6d13b
vDrWool <vdrwool@gmail.com>
2021-03-24 11:03:16 +0300

    Start refactor

--- DataAccess/DataAccess.Static/PostgreRepository.hpp
+++ DataAccess/DataAccess.Static/PostgreRepository.hpp
@@ -1,17 +1,24 @@
 #pragma once
 
+#include "PostgreAdapter.hpp"
 #include "DataAccess/IRepository.hpp"
 
 namespace DataAccess
 {
-class PostgreRepository : public IRepository
-{
-    public:
-    PostgreRepository();
-    virtual ~PostgreRepository() = default;
+    class PostgreRepository : public IRepository
+    {
+    private:
+    
+        std::shared_ptr<PostgreAdapter> mp_dataBaseInstance;
 
-    virtual std::vector<std::string> getAllChannelsList() override final;
-    virtual std::vector<std::string> getMessageHistoryForUser(std::string) override final;
-    virtual void storeMessages(std::vector<std::string>) override final;
-};
+    public:
+
+        PostgreRepository();
+        virtual ~PostgreRepository() = default;
+    
+        virtual std::vector<std::string> getAllChannelsList() override final;
+        virtual std::vector<std::string> getMessageHistoryForUser(std::string) override final;
+        virtual void storeMessages(std::vector<std::string>) override final;
+    
+    };
 }
