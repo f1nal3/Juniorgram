a425cd3b25247ffdef6f4439806930c01f8ccfeb
Husko Maksim <gusko.maksim.n@gmail.com>
2021-04-05 23:16:09 +0300

    message visual representation

--- Client/Client.Qt/ChatWindow.cpp
+++ Client/Client.Qt/ChatWindow.cpp
@@ -5,19 +5,18 @@ ChatWindow::ChatWindow(QWidget* parent) : QWidget(parent)
 {
     QWidget* window = new QWidget;
 
-    textEdit = new TextEdit(window);
-    sendButton = new QPushButton("Send");
+    textEdit     = new TextEdit(window);
+    sendButton   = new QPushButton("Send");
     randomButton = new QPushButton("Bot");
-    chatWidget = new QListWidget(window);
+    chatWidget   = new QListWidget(window);
 
-    QHBoxLayout *layout = new QHBoxLayout;
-    QVBoxLayout *textLayout = new QVBoxLayout;
-    QVBoxLayout *barLayout = new QVBoxLayout;
+    QHBoxLayout* layout     = new QHBoxLayout;
+    QVBoxLayout* textLayout = new QVBoxLayout;
+    QVBoxLayout* barLayout  = new QVBoxLayout;
 
     barLayout->addWidget(randomButton);
     barLayout->addWidget(sendButton);
     barLayout->setAlignment(Qt::AlignBottom);
-    
 
     textLayout->addWidget(chatWidget, 95);
     textLayout->addWidget(textEdit, 5);
@@ -25,7 +24,45 @@ ChatWindow::ChatWindow(QWidget* parent) : QWidget(parent)
     layout->addLayout(textLayout);
     layout->addLayout(barLayout);
 
+    connectButton();
+
     setLayout(layout);
+}
+
+void ChatWindow::connectButton()
+{
+    connect(sendButton, &QPushButton::released, this, &ChatWindow::updateMessagesList_User);
+    connect(randomButton, &QPushButton::released, this, &ChatWindow::updateMessagesList_Bot);
+}
+
+
+void ChatWindow::updateMessagesList_User()
+{
+    newMessage(textEdit->text());
+    textEdit->clear();
+}
+
+void ChatWindow::updateMessagesList_Bot()
+{
+    newMessage("This is message from bot!", "bot");
+}
+
+void ChatWindow::newMessage(QString textMessage, QString userNameMessage)
+{
+    QMessageWidget* myItem = new QMessageWidget(textMessage, userNameMessage);
+    QListWidgetItem* item  = new QListWidgetItem();
+    item->setSizeHint(QSize(0, 120));
+    chatWidget->addItem(item);
+    chatWidget->setItemWidget(item, myItem);
+}
+
+void ChatWindow::newMessage(QString textMessage)
+{
+    QMessageWidget* myItem = new QMessageWidget(textMessage);
+    QListWidgetItem* item  = new QListWidgetItem();
+    item->setSizeHint(QSize(0, 120));
+    chatWidget->addItem(item);
+    chatWidget->setItemWidget(item, myItem);
 }
 
 ChatWindow::~ChatWindow(){
