fa73b4cf36cb71f06b6217d4d1e06d13df7e221b
Husko Maksim <57706182+GuskoMaksim@users.noreply.github.com>
2021-04-06 00:46:04 +0300

    Front message representation  (#118)
    * ui QMessegeWidget
    
    * rebese
    
    * message visual representation
    
    * fix
    
    * fix name
    
    * some fix
    
    * fix MessageWidget
    
    * changes from h to hpp
    
    Co-authored-by: Husko Maksim <gusko.maksim.n@gmail.com>

--- Client/Client.Qt/ChatWindow.hpp
+++ Client/Client.Qt/ChatWindow.hpp
@@ -2,6 +2,7 @@
 #include "Widgets/TextEdit.h"
 #include "Widgets/InputFields.h"
 #include "pch.h"
+#include "MessageWidget.hpp"
 
 class ChatWindow : public QWidget
 {
@@ -10,9 +11,16 @@ public:
     explicit ChatWindow(QWidget* parent = nullptr);
     ~ChatWindow();
 
+public slots:
+    void updateMessagesList_User();
+    void updateMessagesList_Bot();
+
 private:
     QPushButton* sendButton;
     QPushButton* randomButton;
     TextEdit* textEdit; 
     QListWidget* chatWidget;
+    void connectButton();
+    void newMessage(QString textMessage, QString userNameMessage);
+    void newMessage(QString textMessage);
 };
