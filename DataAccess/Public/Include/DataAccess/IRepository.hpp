e65d0237757fe60572b3cb23fc937d4c775787f5
vDrWool <vdrwool@gmail.com>
2021-04-09 11:21:09 +0300

    Const and ref correctness

--- DataAccess/Public/Include/DataAccess/IRepository.hpp
+++ DataAccess/Public/Include/DataAccess/IRepository.hpp
@@ -16,19 +16,19 @@ class IRepository
      * @brief Draft method for getting all channel list from the server.
      * @return channels list as vector of strings.
      */
-    virtual std::vector<std::string> getAllChannelsList() = 0;
+    virtual const std::vector<std::string> getAllChannelsList() = 0;
     /**
      * @brief Draft method for getting history for a specific user. \
      * History is all messages that a marked as "non-delivered" for this user.
      * @param UserID as a string
      * @return List of messages as vector of strings.
      */
-    virtual std::vector<std::string> getMessageHistoryForUser(std::string UserID) = 0;
+    virtual const std::vector<std::string> getMessageHistoryForUser(const std::string& UserID) = 0;
     /**
      * @brief stores messages in repository
      * @param MessagesList as vector of strings.
      */
-    virtual void storeMessages(std::vector<std::string> MessagesList) = 0;
+    virtual void storeMessages(const std::vector<std::string>& MessagesList) = 0;
 
     /**
      * @brief Virtual dtor.
