e8841f996675514b0fe4f5c8b37c31179b23d10e
fisyamaloy <fisyuklol@gmail.com>
2021-04-11 10:24:00 +0300

    fix merge conflicts

--- DataAccess/DataAccess.Static/PostgreRepository.hpp
+++ DataAccess/DataAccess.Static/PostgreRepository.hpp
@@ -16,8 +16,8 @@ namespace DataAccess
         PostgreRepository();
         virtual ~PostgreRepository() = default;
     
-        virtual std::vector<std::string> getAllChannelsList() override final;
-        virtual std::vector<std::string> getMessageHistoryForUser(std::string) override final;
+        virtual const std::vector<std::string> getAllChannelsList() override final;
+        virtual const std::vector<std::string> getMessageHistoryForUser(const std::string& UserID) override final;
         virtual void storeMessage(const MessageWrapper&) override final;
     };
 }
