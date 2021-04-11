b207df81262c650fdcffce59d91e149ff3bacaad
Stanislav <stanislav.daschkevitch@yandex.by>
2021-04-11 13:40:35 +0300

    Documentation

--- Client/Client.Qt/Widgets/BioButton.cpp
+++ Client/Client.Qt/Widgets/BioButton.cpp
@@ -7,6 +7,11 @@
 
 BioButton::BioButton(QImage bioImage, bool inCaption, QWidget* parent) : QWidget(parent)
 {
+    /*
+     * This part set button sizes.
+     * P.S. We didn't have and don't have a designer
+     * so this was the take from Teams desktop :)
+     */
     if (inCaption)
     {
         setFixedWidth(Style::valueDPIScale(46));
@@ -15,15 +20,28 @@ BioButton::BioButton(QImage bioImage, bool inCaption, QWidget* parent) : QWidget
     else
     {
     }
+
+    // Setting up hover animation
+    // P.S. why??
+    // P.S.S why not
     fadeinAnim = new QPropertyAnimation(this, "hoverColor");
     fadeinAnim->setDuration(150);
     fadeinAnim->setEasingCurve(QEasingCurve::InCubic);
     fadeinAnim->setStartValue(QColor(255, 255, 255, 0));
     fadeinAnim->setEndValue(QColor(255, 255, 255, 76));
-    hoverColor  = QColor(255, 255, 255, 0);
-    image       = std::move(bioImage);
+
+    // Setting up background color
+    hoverColor = QColor(255, 255, 255, 0);
+
+    // Setting up profile image
+    image = std::move(bioImage);
+
+    // Now it's all up to eventFilter
     installEventFilter(this);
+
+    // For HoverXXX messages
     setAttribute(Qt::WA_Hover);
+
     // setMouseTracking(true);
 }
 
@@ -33,41 +51,69 @@ bool BioButton::eventFilter(QObject* object, QEvent* event)
 {
     if (object == this)
     {
+        // If mouse is enter then start the hover animation
         if (event->type() == QEvent::HoverEnter)
         {
             fadeinAnim->start();
             return true;
         }
+        // If leaves return background color
+        // P.S. there is no animation :(
         else if (event->type() == QEvent::HoverLeave)
         {
             fadeinAnim->stop();
             hoverColor = fadeinAnim->startValue().value<QColor>();
             update();
+            return true;
         }
+        // On click
         else if (event->type() == QEvent::MouseButtonRelease)
         {
             fadeinAnim->stop();
-            hoverColor       = fadeinAnim->startValue().value<QColor>();
-            auto ev          = dynamic_cast<QMouseEvent*>(event);
-            auto beforepoint = this->geometry().bottomLeft();
-            auto point       = mapToGlobal(this->geometry().bottomLeft());
-            Menu* menu       = new Menu;
+            hoverColor = fadeinAnim->startValue().value<QColor>();
+            // Getting mouse event info
+            auto ev = dynamic_cast<QMouseEvent*>(event);
+
+            /*
+             * Bottom left point of widget in application and on screen
+             * When I was writing this I've thought that screen position is enough
+             * but there was a shift on X
+             */
+            auto localPoint  = this->geometry().bottomLeft();
+            auto globalPoint = mapToGlobal(localPoint);
+
+            // Creating menu
+            // UVAHA: Do not set "this" as a parent
+            Menu* menu = new Menu;
+
+            // Adding options
             menu->addAction("Username: Add format here", []() {});
             menu->addAction("Quit", []() { App::setAppState(AppState::LoginForm); });
+
+            // We need this to happen on LMB
             if (ev->button() == Qt::LeftButton)
             {
+                // Creating popup and setting
                 auto pop = new PopupWidget();
                 pop->setMenu(menu);
-                pop->popup(QPoint(point.x() - beforepoint.x(), point.y() + 1));
+
+                // Now show the menu
+                pop->popup(QPoint(globalPoint.x() - localPoint.x(), globalPoint.y() + 1));
             }
+            return true;
         }
     }
     else
     {
+        /*
+         * If message to not our widget
+         * then reset everything
+         */
         fadeinAnim->stop();
         hoverColor = fadeinAnim->startValue().value<QColor>();
         repaint();
     }
+
     return QObject::eventFilter(object, event);
 }
 
@@ -75,15 +121,22 @@ void BioButton::paintEvent(QPaintEvent* event)
 {
     QPainter p(this);
 
+    // No borders
     p.setPen(Qt::NoPen);
+
+    // Background color
     p.fillRect(0, 0, width(), height(), hoverColor);
-    int di          = Style::valueDPIScale(20);
+
+    // Profile image diameter 20 < 30 (height of button)
+    int di = Style::valueDPIScale(20);
+
+    // Right top offset
     const int hordi = (width() - di) / 2;
     const int verdi = (height() - di) / 2;
+
+    // We need smooth images
     p.setRenderHint(QPainter::SmoothPixmapTransform);
-    p.setRenderHint(QPainter::Antialiasing);
     p.drawImage(QRect(hordi, verdi, di, di), image);
 
     QWidget::paintEvent(event);
 }
-
--- Client/Client.Qt/Widgets/BioButton.hpp
+++ Client/Client.Qt/Widgets/BioButton.hpp
@@ -8,8 +8,18 @@ class BioButton : public QWidget
     Q_OBJECT
     Q_PROPERTY(QColor hoverColor READ getHoverColor WRITE setHoverColor)
 public:
+    /**
+     * Constructor of bio button
+     * @param bioImage Profile image
+     * @param inCaption Must be set true for now.
+     * @param parent Parent of widget
+     */
     explicit BioButton(QImage bioImage, bool inCaption = true, QWidget* parent = nullptr);
 
+    /**
+     * Changes profile image (locally)
+     * @param newImage
+     */
     void setImage(QImage newImage);
 
 protected:
@@ -17,11 +27,19 @@ protected:
     void paintEvent(QPaintEvent* event) override;
 
 private:
+    /**
+     * Changes button background color if in title bar
+     * @param newColor New background color
+     */
     void setHoverColor(QColor newColor)
     {
         hoverColor = std::move(newColor);
         repaint();
     }
+    /**
+     * Returns current background color
+     * @return Current background color
+     */
     QColor getHoverColor() { return hoverColor; }
 
 private:
