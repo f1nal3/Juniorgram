ff4512de3f31b393dae2a12f18d06fa6bb2d26d5
andrewdbsk <andrewdbsk@gmail.com>
2021-04-13 18:24:46 +0300

    Comment formatting fix

--- DataAccess/DataAccess.Debug/FileDB.hpp
+++ DataAccess/DataAccess.Debug/FileDB.hpp
@@ -13,16 +13,19 @@
 
 namespace DataAccess
 {
-    /** @class FileDB
-     *  @brief A simple file database for debugging.
-     *  @details Each database instance is constructed with a name as constructor's argument. This name is used as a name for working directory
-     *              of database (subdirectory of executable). Implementation provides no check for names already in use. 
+    /** @class      FileDB
+    * 
+     *  @brief      A simple file database for debugging.
+     * 
+     *  @details    Each database instance is constructed with a name as constructor's argument. This name is used as a name for working directory
+     *              of database (subdirectory of executable's folder). Implementation provides no check for names already in use. 
      *              Each table is stored in a subdirectory named after the table. Table directory contains "properties.json" file with 
-     *              column type information, and "row_N.json" files store actual data. Database is thread-safe,
-     *               but can only serve one thread at a time.
-     *  @syntax Valid indentifier (instance/table/column name) can't start with a digit, and can only contain letters, digits 
-     *           and underscores (as in Postgre, but without '$')
-     *          Suppored types are : int, unsigned, bool, float and string. All numeric types are internally 64-bit by default.
+     *              column type information, and "row_N.json" files that store actual data. Database is thread-safe,
+     *              but can only serve one thread at a time.
+     * 
+     *  @syntax     Valid indentifier (instance/table/column name) can't start with a digit, and can only contain letters, digits 
+     *              and underscores (as in Postgre, but without '$')
+     *              Suppored types are : int, unsigned, bool, float and string. All numeric types are internally 64-bit by default.
      */
 class FileDB
 {
