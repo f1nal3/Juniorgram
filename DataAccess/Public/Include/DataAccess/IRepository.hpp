872003f37e326dbd66bdd380460c6494a7915443
Renata <50527376+HRenata@users.noreply.github.com>
2021-04-16 17:41:03 +0300

    Fix for server build. Removed const from return value in Repository functions (#142)
    * Fix for server build. Removed const from return value in Repository functions
    
    * Fix for suppressWarning

--- DataAccess/Public/Include/DataAccess/IRepository.hpp
+++ DataAccess/Public/Include/DataAccess/IRepository.hpp
@@ -1,29 +1,29 @@
 #pragma once
+#include <Network/Primitives.hpp>
 #include <string>
 #include <vector>
-#include <Network/Primitives.hpp>
 
 namespace DataAccess
 {
-    /** @class IRepository
-     *  @brief An interface for repository
-     *  @details see https://github.com/f1nal3/Juniorgram/wiki/IRepository-scheme for additional details
-     */
+/** @class IRepository
+ *  @brief An interface for repository
+ *  @details see https://github.com/f1nal3/Juniorgram/wiki/IRepository-scheme for additional details
+ */
 class IRepository
 {
-    public:
+public:
     /**
      * @brief Draft method for getting all channel list from the server.
      * @return channels list as vector of strings.
      */
-    virtual const std::vector<std::string> getAllChannelsList() = 0;
+    virtual std::vector<std::string> getAllChannelsList() = 0;
     /**
      * @brief Draft method for getting history for a specific user. \
      * History is all messages that a marked as "non-delivered" for this user.
      * @param UserID as a string
      * @return List of messages as vector of strings.
      */
-    virtual const std::vector<std::string> getMessageHistoryForUser(const std::string& UserID) = 0;
+    virtual std::vector<std::string> getMessageHistoryForUser(const std::string& UserID) = 0;
     /**
      * @brief Draft method for storing user's message in repository
      * @param message as Network::UserMessage.
@@ -34,4 +34,4 @@ class IRepository
      */
     virtual ~IRepository() = default;
 };
-}
+}  // namespace DataAccess
