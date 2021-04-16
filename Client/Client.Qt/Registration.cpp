b3ffe264d751947ee7e7d33ae60af001eb244b3b
Husko Maksim <gusko.maksim.n@gmail.com>
2021-04-16 16:33:31 +0300

    add del elem

---
+++ Client/Client.Qt/Registration.cpp
@@ -0,0 +1,29 @@
+#include "Registration.hpp"
+
+#include "App.hpp"
+
+Registration::Registration(QWidget* parent) : QWidget(parent)
+{
+    usernameLineEdit       = std::make_unique<FlatInput>("Username");
+    passwordLineEdit       = std::make_unique<FlatInput>("Password", true);
+    passwordRepeatLineEdit = std::make_unique<FlatInput>("Repeat password", true);
+    registrationButton     = std::make_unique<FlatButton>("Create account");
+    back                   = std::make_unique<FlatButton>("Back");
+
+    QObject::connect(back.get(), &FlatButton::pressed,
+                     []() { App::setAppState(AppState::LoginForm); });
+
+    gridLayout = std::make_unique<QGridLayout>();
+    gridLayout->addWidget(usernameLineEdit.get(), 0, 0);
+    gridLayout->addWidget(passwordLineEdit.get(), 1, 0);
+    gridLayout->addWidget(passwordRepeatLineEdit.get(), 2, 0);
+    gridLayout->addWidget(registrationButton.get(), 3, 0);
+    gridLayout->addWidget(back.get(), 4, 0);
+    this->setLayout(gridLayout.get());
+}
+
+void Registration::keyPressEvent(QKeyEvent* event)
+{
+    if ((event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return))
+        App::setAppState(AppState::LoginForm);
+}
---
+++ Client/Client.Qt/Registration.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "Widgets/FlatButton.hpp"
+#include "Widgets/InputFields.hpp"
+#include "pch.hpp"
+
+class Registration : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Registration(QWidget* parent = nullptr);
+
+private:
+    std::unique_ptr<FlatInput> usernameLineEdit;
+    std::unique_ptr<FlatInput> passwordLineEdit;
+    std::unique_ptr<FlatInput> passwordRepeatLineEdit;
+    std::unique_ptr<FlatButton> registrationButton;
+    std::unique_ptr<FlatButton> back;
+    std::unique_ptr<QGridLayout> gridLayout;
+    void keyPressEvent(QKeyEvent* event);
+};
