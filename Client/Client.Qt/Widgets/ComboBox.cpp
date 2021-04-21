f11071eac3cef85b37fdf808f71146671e52dbda
Husko Maksim <gusko.maksim.n@gmail.com>
2021-04-21 17:26:06 +0300

    fix

--- Client/Client.Qt/Widgets/ComboBox.cpp
+++ Client/Client.Qt/Widgets/ComboBox.cpp
@@ -2,12 +2,13 @@
 
 #include "Style/Style.hpp"
 
-ComboBox::ComboBox(QWidget* parent) : QComboBox(parent) {
+ComboBox::ComboBox(QWidget* parent) : QComboBox(parent)
+{
     setFont(QFont("NotoSans", Style::valueDPIScale(12), 55));
     setStyleSheet(
         QString("QComboBox { "
                 "border: 0px;"
                 "background-color: rgba(0,0,0,0);"
                 "color: white;"
-                "}")); 
+                "}"));
 }
