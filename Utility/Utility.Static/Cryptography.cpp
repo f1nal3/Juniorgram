2c1c5d30a06d7d94ba8d4580fee2be8e3713a3ed
Danil Fisiuk <61632356+fisyamaloy@users.noreply.github.com>
2021-04-28 23:24:52 +0300

    Cryptography (#175)
    * Cryptography
    
    * conan package was included
    
    * a
    
    * Parrern factory method
    
    * some fix
    
    * rename of namespace
    
    * build fix
    
    * build fix
    
    * Cryptography
    
    * conan package was included
    
    * a
    
    * Parrern factory method
    
    * some fix
    
    * rename of namespace
    
    * build fix
    
    * build fix
    
    * garbage remove

---
+++ Utility/Utility.Static/Cryptography.cpp
@@ -0,0 +1,75 @@
+#include <cryptopp/hex.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/osrng.h>
+
+#include <memory>
+#include <string>
+
+namespace SymmetricCipher
+{
+using namespace CryptoPP;
+
+struct ICipher
+{
+    virtual std::string generateKey()                                                        = 0;
+    virtual std::string encrypt(const std::string& data, const std::string& cipherKey)       = 0;
+    virtual std::string decrypt(const std::string& cipherData, const std::string& cipherKey) = 0;
+    virtual ~ICipher() = default;
+};
+
+struct ICiphersFactory
+{
+    virtual std::shared_ptr<ICipher> create() = 0;
+    virtual ~ICiphersFactory()                = default;
+};
+
+struct AESCipher : ICipher
+{
+    std::string generateKey()
+    {
+        AutoSeededRandomPool rnd;
+
+        SecByteBlock bytes(0x0, AES::DEFAULT_KEYLENGTH);
+        rnd.GenerateBlock(bytes, bytes.size());
+
+        return std::string(reinterpret_cast<const char*>(bytes.BytePtr()), bytes.size());
+    }
+
+    std::string encrypt(const std::string& data, const std::string& key)
+    {
+        ECB_Mode<AES>::Encryption encryptor;
+
+        const byte* pKey = reinterpret_cast<const byte*>(&key[0]);
+        encryptor.SetKey(pKey, key.size());
+
+        std::string encryptedData;
+
+        StringSource(data, true,
+            new StreamTransformationFilter(encryptor,
+                new HexEncoder(new StringSink(encryptedData))));
+
+        return encryptedData;
+    }
+
+    std::string decrypt(const std::string& cipherData, const std::string& key)
+    {
+        ECB_Mode<AES>::Decryption decryptor;
+
+        const byte* pKey = reinterpret_cast<const byte*>(&key[0]);
+        decryptor.SetKey(pKey, key.size());
+
+        std::string decryptedData;
+
+        StringSource(cipherData, true,
+            new HexDecoder(
+                new StreamTransformationFilter(decryptor, new StringSink(decryptedData))));
+
+        return decryptedData;
+    }
+};
+
+struct AESFactory : ICiphersFactory
+{
+    std::shared_ptr<ICipher> create() override { return std::make_shared<AESCipher>(); }
+};
+}  // namespace SymmetricCipher
