55d573fb121c4733e7fb7f54436729784fc22939
Husko Maksim <gusko.maksim.n@gmail.com>
2021-05-07 23:20:26 +0300

    added the custom QDateTimeEdit class

---
+++ Client/Client.Qt/Widgets/DateTimeEdit.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "pch.hpp"
+
+/** @class QDateTimeEdit
+ *  @brief Custom style for QDateTimeEdit object
+ */
+class DateTimeEdit : public QDateTimeEdit
+{
+public:
+    DateTimeEdit(QWidget* parent = nullptr);
+};
