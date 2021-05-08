7bef07955dc4fe1df8c84976a571f512fee9a835
Husko Maksim <gusko.maksim.n@gmail.com>
2021-05-08 14:45:52 +0300

    added documentation for new features and fixed old documentation

--- Client/Client.Qt/Widgets/MessageWidget.hpp
+++ Client/Client.Qt/Widgets/MessageWidget.hpp
@@ -32,38 +32,45 @@ public:
     void setMessageText(QString newMessage);
     /**
      * @brief Method for method for changing the message.
-     * @param new text message as string of String.
+     * @param new text message as string of std::string.
      */
     void setStdMessageText(std::string newMessage);
     /**
-     * @brief Method for method for changing the nickname of the message sender.
+     * @brief Method for changing the nickname of the message sender.
      * @param new nickname as string of QString.
      */
     void setUserName(QString newUserName);
     /**
-     * @brief Method for method for changing the nickname of the message sender.
-     * @param new nickname as string of String.
+     * @brief Method for changing the nickname of the message sender.
+     * @param new nickname as string of std::string.
      */
     void setStdUserName(std::string newUserName);
-
+    /**
+     * @brief Method for changing the data and time of the message sender.
+     * @param new datatime of QDateTime.
+     */
     void setDateTime(QDateTime newDataTime);
+    /**
+     * @brief Method for changing the data and time of the message sender.
+     * @param new datatime of std::time_t.
+     */
     void setStdTime_tDateTime(std::time_t newDataTime);
-
     /**
      * @brief constructor for displaying a message from a user on the screen.
      * @param text of message as string of QStrings.
      * @param nickname as string of QStrings.
      * @param object of the QListWidgetItem type.
+     * @param boolean indicator of a deleted message.
      * @param object of the QWidget type.
      */
     MessageWidget(QString textMessage, QString nameOfUser, QListWidgetItem* Item,
                   bool deletedMessage, QWidget* parent = nullptr);
     /**
      * @brief constructor for displaying a message from a user on the screen.
-     * @param text of message as string of Strings.
-     * @param nickname as string of Strings.
+     * @param text of message as string of std::strings.
+     * @param nickname as string of std::strings.
      * @param object of the QListWidgetItem type.
-     * @param object of the QWidget type.
+     * @param boolean indicator of a deleted message.
      */
     MessageWidget(std::string textMessage, std::string nameOfUser, QListWidgetItem* Item,
                   bool deletedMessage);
@@ -71,11 +78,13 @@ public:
      * @brief constructor for displaying a message from a user on the screen.
      * @param text of message as string of QStrings.
      * @param object of the QListWidgetItem type.
+     * @param boolean indicator of a deleted message.
      */
     MessageWidget(QString textMessage, QListWidgetItem* Item, bool deletedMessage);
     /**
      * @brief constructor for displaying a message from a user on the screen.
      * @param object of the QListWidgetItem type.
+     * @param boolean indicator of a deleted message.
      */
     explicit MessageWidget(QListWidgetItem* Item, bool deletedMessage);
     /**
