ee11da843a1afc15980dc9bb1cf63e7396e86ace
Stanislav <stanislav.daschkevitch@yandex.by>
2021-06-10 00:01:21 +0300

    fixes

---
+++ Client/Client.Qt/Style/Palette.cpp
@@ -0,0 +1,77 @@
+// WARNING! All changes made in this file will be lost!
+// This file was created automatically
+#include "Palette.h"
+
+namespace {
+
+bool inited = false;
+
+class Module_Palette : public Style::internal::ModuleBase {
+public:
+	Module_Palette() { Style::internal::registerModule(this); }
+
+	void start(int scale) override {
+		Style::internal::init_Palette(scale);
+	}
+};
+Module_Palette registrator;
+
+Style::palette _palette;
+
+} // namespace
+
+namespace st {
+const Style::color &transparent(_palette.get_transparent()); // special color
+const Style::color &windowActiveTextFg(_palette.get_windowActiveTextFg());
+const Style::color &window(_palette.get_window());
+const Style::color &windowColor(_palette.get_windowColor());
+const Style::color &windowColorOver(_palette.get_windowColorOver());
+const Style::color &titleButton(_palette.get_titleButton());
+const Style::color &titleButtonOver(_palette.get_titleButtonOver());
+const Style::color &closeButtonC(_palette.get_closeButtonC());
+const Style::color &closeButtonCOver(_palette.get_closeButtonCOver());
+} // namespace st
+
+namespace Style {
+
+
+int palette::indexOfColor(Style::color c) const {
+	auto start = data(0);
+	if (c._data >= start && c._data < start + kCount) {
+		return static_cast<int>(c._data - start);
+	}
+	return -1;
+}
+
+color palette::colorAtIndex(int index) const {
+	assert(_ready);
+	assert(index >= 0 && index < kCount);
+	return _colors[index];
+}
+
+void palette::finalize() {
+	if (_ready) return;
+	_ready = true;
+
+	compute(0, -1, { 255, 255, 255, 0}); // special color
+	compute(1, -1, { 255, 255, 255, 255 });
+	compute(2, -1, { 50, 50, 50, 255 });
+	compute(3, -1, { 64, 65, 66, 255 });
+	compute(4, -1, { 82, 83, 84, 255 });
+	compute(5, -1, { 255, 255, 255, 0 });
+	compute(6, -1, { 255, 255, 255, 76 });
+	compute(7, -1, { 232, 17, 35, 0 });
+	compute(8, -1, { 232, 17, 35, 76 });
+
+
+namespace internal {
+
+void init_Palette(int scale) {
+	if (inited) return;
+	inited = true;
+
+	_palette.finalize();
+}
+
+} // namespace internal
+} // namespace Style
