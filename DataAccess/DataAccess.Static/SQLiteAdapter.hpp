b270b41c069a2eb712a10155250d40443df59968
Svidunovich Yaraslau <q77qw2000@gmail.com>
2021-06-10 16:51:27 +0300

    Some code

--- DataAccess/DataAccess.Static/SQLiteAdapter.hpp
+++ DataAccess/DataAccess.Static/SQLiteAdapter.hpp
@@ -3,16 +3,27 @@
 #include <QtSql/QSql>
 #include <QtSql/QSqlDatabase>
 #include <QtSql/QSqlQuery>
+
 #include <DataAccess/IAdapter.hpp>
+#include <Utility/Exception.hpp>
+
+#include <memory>
+#include <mutex>
 
 namespace  DataAccess
 {
 class SQLiteAdapter : public IAdapter
 {
+public:
+    inline static std::mutex mtx{};
+    inline static std::shared_ptr<SQLiteAdapter> sqlite_instance{};
+    inline static const QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    static std::shared_ptr<SQLiteAdapter> Instance(const QSqlDatabase& database);
 private:
-    QSqlDatabase db;
+    std::unique_ptr<QSqlDatabase> db_connection;
 protected:
-    SQLiteAdapter();
+    SQLiteAdapter(const QSqlDatabase& database)
+        : db_connection{std::make_unique<QSqlDatabase>(database)} {}
 public:
     SQLiteAdapter(const SQLiteAdapter& other) = delete;
     SQLiteAdapter& operator=(const SQLiteAdapter& other) = delete;
