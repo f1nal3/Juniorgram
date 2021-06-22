7cf6c367159c3a93fe27c385131b3e93f501a053
Husko Maksim <gusko.maksim.n@gmail.com>
2021-06-22 23:50:36 +0300

    refactoring Menu

--- Client/Client.Qt/Widgets/Menu.cpp
+++ Client/Client.Qt/Widgets/Menu.cpp
@@ -10,16 +10,18 @@ QAction* CreateAction(QWidget* actionparent, const QString& text, std::function<
 Menu::Menu(QWidget* parent) : QWidget(parent) {}
 
 void Menu::paintEvent(QPaintEvent* event) { QWidget::paintEvent(event); }
+
 void Menu::addAction(const QString& text, std::function<void()>&& callback)
 {
     auto action = CreateAction(this, text, static_cast<std::function<void()>&&>(callback));
-    auto Item   = std::make_unique<MenuItem>(text, action, this);
+    auto Item   = new MenuItem(text, action, this);
     int top     = _items.empty() ? 0 : _items.back()->y() + _items.back()->height();
     Item->move(0, top);
     Item->show();
     _items.push_back(std::move(Item));
     this->setFixedSize(256, _items.back()->y() + _items.back()->height());
 }
+
 void Menu::hideEvent(QHideEvent* event)
 {
     if (parent())
