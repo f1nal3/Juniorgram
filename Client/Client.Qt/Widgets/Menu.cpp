1e60b10f37d1a48cbf0f173db5b1bb953c71e064
Husko Maksim <gusko.maksim.n@gmail.com>
2021-06-24 15:53:24 +0300

    fix Menu

--- Client/Client.Qt/Widgets/Menu.cpp
+++ Client/Client.Qt/Widgets/Menu.cpp
@@ -1,4 +1,5 @@
 #include "Menu.hpp"
+
 QAction* CreateAction(QWidget* actionparent, const QString& text, std::function<void()>&& callback)
 {
     const auto action = new QAction(text, actionparent);
@@ -7,26 +8,33 @@ QAction* CreateAction(QWidget* actionparent, const QString& text, std::function<
     return action;
 }
 
-Menu::Menu(QWidget* parent) : QWidget(parent) {}
+Menu::Menu(QWidget* parent) : QWidget(parent) { _items = new std::vector<MenuItem*>; }
 
-void Menu::paintEvent(QPaintEvent* event) { QWidget::paintEvent(event); }
+void Menu::paintEvent(QPaintEvent* paintEvent) { QWidget::paintEvent(paintEvent); }
+
+Menu::~Menu()
+{
+    for (auto item : *_items)
+        delete item;
+    delete _items;
+}
 
 void Menu::addAction(const QString& text, std::function<void()>&& callback)
 {
     auto action = CreateAction(this, text, static_cast<std::function<void()>&&>(callback));
     auto Item   = new MenuItem(text, action, this);
-    int top     = _items.empty() ? 0 : _items.back()->y() + _items.back()->height();
+    int top     = _items->empty() ? 0 : _items->back()->y() + _items->back()->height();
     Item->move(0, top);
     Item->show();
-    _items.push_back(std::move(Item));
-    this->setFixedSize(256, _items.back()->y() + _items.back()->height());
+    _items->push_back(std::move(Item));
+    this->setFixedSize(256, _items->back()->y() + _items->back()->height());
 }
 
-void Menu::hideEvent(QHideEvent* event)
+void Menu::hideEvent(QHideEvent* hideEvent)
 {
     if (parent())
     {
         parentWidget()->hide();
     }
-    QWidget::hideEvent(event);
+    QWidget::hideEvent(hideEvent);
 }
