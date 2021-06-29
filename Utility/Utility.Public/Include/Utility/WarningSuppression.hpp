7469244682fb54c7e1c56a73e2991ae5c39e764a
vDrWool <vdrwool@gmail.com>
2021-06-29 18:25:50 +0300

    WarningSuppression fix

--- Utility/Utility.Public/Include/Utility/WarningSuppression.hpp
+++ Utility/Utility.Public/Include/Utility/WarningSuppression.hpp
@@ -5,12 +5,12 @@
 													  __pragma(warning(disable: MSVC_CODE))
 #	define restoreWarning __pragma(warning(pop))
 #elif __GNUC__ 
-#	define _doPragma(x) _Pragma(x)
+#	define _doPragma(x) _Pragma(#x)
 #	define suppressWarning(MSVC_CODE, GCC_CLANG_TEXT) _doPragma(GCC diagnostic push) \
 													  _doPragma(GCC diagnostic ignored GCC_CLANG_TEXT)
 #	define restoreWarning _doPragma(GCC diagnostic pop)
 #elif __clang__
-#	define _doPragma(x) _Pragma(x)
+#	define _doPragma(x) _Pragma(#x)
 #	define suppressWarning(MSVC_CODE, GCC_CLANG_TEXT) _doPragma(clang diagnostic push) \
 													  _doPragma(clang diagnostic ignored GCC_CLANG_TEXT)
 #	define restoreWarning _doPragma(clang diagnostic pop)
