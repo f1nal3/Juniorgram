aced336fe7c90485d0c9dd5654e15b035146bcbc
Данил Фисюк <fisyuklol@gmail.com>
2021-07-05 15:53:01 +0300

    fix build

--- DataAccess/DataAccess.Test/UsersAmountFunctions.hpp
+++ DataAccess/DataAccess.Test/UsersAmountFunctions.hpp
@@ -1,4 +1,5 @@
 #include <DataAccess.Static/PostgreTable.hpp>
+#include <Utility/WarningSuppression.hpp>
 
 std::uint16_t findUsersAmountWithSameTableAttribute(const std::string& condition)
 {
@@ -9,20 +10,20 @@ std::uint16_t findUsersAmountWithSameTableAttribute(const std::string& condition
                                     ->execute();
 
     return RECORDS_AMOUNT.value()[0][0].as<std::uint16_t>();
-};
+}
 
 std::uint16_t findUsersAmountWithSameLogin(const std::string& login)
 {
     return findUsersAmountWithSameTableAttribute("login='" + login + "'");
-};
+}
 
 std::uint16_t findUsersAmountWithSameEmail(const std::string& email)
 {
     return findUsersAmountWithSameTableAttribute("email='" + email + "'");
-};
+}
 
 std::uint16_t findUsersAmountWithAllSameData(const Network::RegistrationInfo& ri)
 {
     return findUsersAmountWithSameTableAttribute("email='" + ri.email + "' and login='" + ri.login +
                                                  "' and password_hash='" + ri.passwordHash + "'");
-};
+}
