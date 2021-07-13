f03c00e75b7038b92d0d4014a19806c6b51d1775
Данил Фисюк <fisyuklol@gmail.com>
2021-07-13 18:55:15 +0300

    change header to string_view

--- Client/Client.Core/Client.Core.Static/ServerInfo.hpp
+++ Client/Client.Core/Client.Core.Static/ServerInfo.hpp
@@ -1,7 +1,7 @@
-#include <string>
+#include <string_view>
 
 namespace ServerInfo
 {
 	constexpr std::string_view address = "104.40.239.183";
-	const std::uint16_t port  = 65001;
+	const		   std::uint16_t port  = 65001;
 }  // namespace ServerInfo
