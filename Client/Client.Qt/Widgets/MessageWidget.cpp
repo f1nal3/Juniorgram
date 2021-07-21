300975764954afd4e1effb3cfcd855af1c934a26
Husko Maksim <gusko.maksim.n@gmail.com>
2021-07-21 17:43:22 +0300

    fix

--- Client/Client.Qt/Widgets/MessageWidget.cpp
+++ Client/Client.Qt/Widgets/MessageWidget.cpp
@@ -11,10 +11,10 @@
 MessageWidget::MessageWidget(QWidget* history, QString message, int userId, int messageId, qint64 utc, QString username, const Style::MessageWidget& st)
     : QWidget(history),
     _messageText(std::move(message)),
-    _username(std::move(username)),
-    _datetime(QDateTime::fromSecsSinceEpoch(utc)),
     _userId(std::move(userId)),
     _messageId(std::move(messageId)),
+    _username(std::move(username)),
+    _datetime(QDateTime::fromSecsSinceEpoch(utc)),
     _st(st)
 {
     setContentsMargins(QMargins(_st.radius, _st.radius, _st.radius, _st.radius));
