f5c93789b6738a85284166e1cc159b217c6a3aed
Svidunovich Yaraslau <q77qw2000@gmail.com>
2021-07-23 17:56:06 +0300

    fix user stretch factor

--- Client/Client.Qt/ChatWindow.cpp
+++ Client/Client.Qt/ChatWindow.cpp
@@ -14,19 +14,15 @@ ChatWindow::ChatWindow(QWidget* parent) : QWidget(parent)
     _mainLayout->setContentsMargins(0, 0, 0, 0);
     _mainLayout->addWidget(_channelListWidget.get());
     _mainLayout->addWidget(_chatSwitchWidget.get());
+    _mainLayout->setStretchFactor(0, 20);
+    _mainLayout->setStretchFactor(1, 80);
 
     connect(_channelListWidget->getChannelList().get(), &ListWidget::itemPressed,
             [&](){
             if(_chatSwitchWidget->count() == _channelListWidget->getChannelList()->currentRow())
-            {
                 _chatSwitchWidget->insertWidget(_channelListWidget->getChannelList()->currentRow(), new ChatWidget());
-                _mainLayout->setStretchFactor(1, 80);
-            }
             else
-            {
                 _chatSwitchWidget->setCurrentIndex(_channelListWidget->getChannelList()->currentRow());
-                _mainLayout->setStretchFactor(1, 80);
-            }
     });
 }
 
