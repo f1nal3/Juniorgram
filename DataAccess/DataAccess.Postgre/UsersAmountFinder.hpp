3970c6f80d17afd464559a633d30d08b2c9438c3
Danil Fisiuk <61632356+fisyamaloy@users.noreply.github.com>
2021-07-27 19:45:01 +0300

    PostgreRepository documentation (#318)
    * documentation adding
    
    * append *

--- DataAccess/DataAccess.Postgre/UsersAmountFinder.hpp
+++ DataAccess/DataAccess.Postgre/UsersAmountFinder.hpp
@@ -2,6 +2,10 @@
 #include "PostgreTable.hpp"
 #include <Network/Primitives.hpp>
 
+/** @class UsersAmountFinder.
+ *   @brief UsersAmountFinder class.
+ *   @details This class lets us find users amount in DataBase by some conditions.
+ */
 class UsersAmountFinder
 {
 protected:
@@ -16,11 +20,27 @@ public:
 
     virtual ~UsersAmountFinder() = default;
 
+    /** @brief Common method which let us pull from DB users amount by some condition.
+     *   @params condition - SQL condition.
+     *   @return users amount with the same table attribute.
+     */
     std::uint16_t findUsersAmountWithSameTableAttribute(const std::string& condition) const;
 
+    /** @brief Find users amount in DB by login.
+     *   @params std::string& - user's login.
+     *   @return users amount with the same login.
+     */
     std::uint16_t findUsersAmountWithSameLogin(const std::string& login) const;
 
+    /** @brief Find users amount in DB by email.
+     *   @params std::string& - user's email.
+     *   @return users amount with the same email.
+     */
     std::uint16_t findUsersAmountWithSameEmail(const std::string& email) const;
 
+    /** @brief Find all the same users.
+     *   @params Network::RegistrationInfo& - users info.
+     *   @return users amount with the same users data.
+     */
     std::uint16_t findUsersAmountWithAllSameData(const Network::RegistrationInfo& ri) const;
 };
