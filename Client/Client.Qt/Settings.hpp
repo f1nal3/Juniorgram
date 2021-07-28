4c4e4fb335c3e7c39d181dfb4885f1f3441004c8
Olga Tratsiuk <otratsiuk@gmail.com>
2021-07-28 19:30:18 +0300

    Add documentation

--- Client/Client.Qt/Settings.hpp
+++ Client/Client.Qt/Settings.hpp
@@ -3,6 +3,9 @@
 #include <QSettings>
 #include <memory>
 
+/** @class Settings
+ *  @brief Persistent settings
+ */
 class Settings : public QSettings{
 private:
     Settings();
@@ -14,10 +17,29 @@ private:
     std::unique_ptr<QSettings> settings;
 
 public:
+    /**
+     * @brief Method for getting reference to Settings instance.
+     * @return Reference to Settings instance.
+     */
     static Settings &getInstance();
+    /**
+     * @brief Destructor.
+     */
     ~Settings(){};
+    /**
+     * @brief Draft method for writting settings keys and /
+     * values to .config file.
+     */
     void writeSettings();
+    /**
+     * @brief Draft method for setting font size value
+     * @param size as int.
+     */
     void setFontSize(int size);
+    /**
+     * @brief Draft method for getting font size value
+     * @return fontSize as int.
+     */
     int getFontSize();
 
 private:
