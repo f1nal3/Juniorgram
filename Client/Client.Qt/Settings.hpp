b6f60fdcfde4e6b084d93233dd5854a7ad1e8210
Olga Tratsiuk <55056330+otratsiuk@users.noreply.github.com>
2021-07-28 20:32:31 +0300

    Settings manager (#324)
    * Add settings
    
    * Add documentation

---
+++ Client/Client.Qt/Settings.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <QApplication>
+#include <QSettings>
+#include <memory>
+
+/** @class Settings
+ *  @brief Persistent settings
+ */
+class Settings : public QSettings{
+private:
+    Settings();
+    Settings(const Settings&) = delete;
+    Settings(Settings&&) = delete;
+    Settings& operator=(const Settings&) = delete;
+    Settings& operator=(Settings&&) = delete;
+    static std::unique_ptr<Settings> instance;
+    std::unique_ptr<QSettings> settings;
+
+public:
+    /**
+     * @brief Method for getting reference to Settings instance.
+     * @return Reference to Settings instance.
+     */
+    static Settings& getInstance();
+    /**
+     * @brief Destructor.
+     */
+    ~Settings(){};
+    /**
+     * @brief Draft method for writting settings keys and /
+     * values to .config file.
+     */
+    void writeSettings();
+    /**
+     * @brief Draft method for setting font size value
+     * @param size as int.
+     */
+    void setFontSize(int size);
+    /**
+     * @brief Draft method for getting font size value
+     * @return fontSize as int.
+     */
+    int getFontSize();
+
+private:
+    int _fontSize;
+};
