694b6a77e55c447ecd92a83ff1ab75d15c8ffdc7
StasDash <stanislav.daschkevitch@yandex.by>
2021-07-28 16:16:44 +0300

    fixes

--- Client/Client.Qt/MainWidget.cpp
+++ Client/Client.Qt/MainWidget.cpp
@@ -22,6 +22,7 @@ bool isCompositionEnabled()
 
 bool MainWidget::nativeEvent(const QByteArray& eventType, void* message, long* result)
 {
+    return false;
     Q_UNUSED(eventType)
     MSG* msg = static_cast<MSG*>(message);
 
