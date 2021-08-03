766054fbbe15c6cdd4843b7c9886abe71913c2c0
Данил Фисюк <fisyuklol@gmail.com>
2021-08-03 13:30:20 +0300

    mb final commit

--- DataAccess/DataAccess.Postgre/DataBaseOptions.hpp
+++ DataAccess/DataAccess.Postgre/DataBaseOptions.hpp
@@ -4,7 +4,7 @@
 namespace DBOptions
 {
     constexpr std::string_view real = 
-        "dbname=juniorgram user=postgres hostaddr=127.0.0.1 port=5432 password=123";
+        "dbname=juniorgram user=postgres hostaddr=127.0.0.1 port=5432";
 
     constexpr std::string_view test =
         "hostaddr=104.40.239.183 port=5432 dbname=test user=test_user password=qwerty";
