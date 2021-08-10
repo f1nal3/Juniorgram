0e7fcf6531511f7b8c536164eecec047b8115a7b
Hou5ton <eugene.skrobot@gmail.com>
2021-08-10 16:12:31 +0300

    fix

--- Utility/Utility.Static/Logger.hpp
+++ Utility/Utility.Static/Logger.hpp
@@ -146,6 +146,15 @@ private:
     * @return curent thread in string format
     */
     static std::string threadID();
+
+    template <typename TP>
+    std::time_t to_time_t(TP tp)
+    {
+        using namespace std::chrono;
+        auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now() + system_clock::now());
+        return system_clock::to_time_t(sctp);
+    }
+
     /**
     * @brief Wraps the value of a variable
     * @return [value]
