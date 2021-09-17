5c8789a2f645fbc3865bd6c201a590b42dfe0ee9
vDrWool <vdrwool@gmail.com>
2021-09-17 19:22:00 +0300

    Fix -fpermissive for gcc- warning

--- DataAccess/DataAccess.SQLite/LiteClientRepository.hpp
+++ DataAccess/DataAccess.SQLite/LiteClientRepository.hpp
@@ -1,11 +1,14 @@
 #pragma once
 #include "LiteAdapter.hpp"
 #include <Public/Include/DataAccess/AbstarctRepositoryContainer.hpp>
+#include <Utility/WarningSuppression.hpp>
 
 namespace DataAccess
 {
 	class LiteServerRepository : public AbstarctRepositoryContainer {};
 
+	suppressWarning(0, "-fpermissive")
 	std::shared_ptr<IAdapter> LiteServerRepository::_adapter = LiteAdapter::getInstance<LiteAdapter>();
 	std::map<LiteServerRepository::RealType, LiteServerRepository::IType> LiteServerRepository::_container = {};
+	restoreWarning
 }
