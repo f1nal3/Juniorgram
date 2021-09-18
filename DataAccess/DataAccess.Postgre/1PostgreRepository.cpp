00e85b6cc8b5cda54ffc0905f51d48f855c797de
Svidunovich Yaraslau <q77qw2000@gmail.com>
2021-09-18 18:11:15 +0300

    fix display username

--- DataAccess/DataAccess.Postgre/1PostgreRepository.cpp
+++ DataAccess/DataAccess.Postgre/1PostgreRepository.cpp
@@ -32,9 +32,12 @@ std::vector<Network::MessageInfo> PostgreRepository::getMessageHistoryForUser(co
     auto messageHistoryRow = pTable->Select()
                                    ->columns({"*"})
                                    ->join(Utility::SQLJoinType::J_INNER, "channel_msgs", "channel_msgs.msg_id = msgs.msg_id")
-                                   ->join(Utility::SQLJoinType::J_INNER, "users", "users.id = msgs.sender_id")
                                    ->Where("channel_msgs.channel_id = " + std::to_string(channelID))
                                    ->execute();
+
+    pTable->changeTable("users");
+    auto logins = pTable->Select()->columns({"*"})->execute();
+
 
     if (messageHistoryRow.has_value())
     {
@@ -46,7 +49,7 @@ std::vector<Network::MessageInfo> PostgreRepository::getMessageHistoryForUser(co
             mi.senderID = messageHistoryRow.value()[i][1].as<std::uint64_t>();
             mi.time     = messageHistoryRow.value()[i][2].as<std::string>();
             mi.message  = messageHistoryRow.value()[i][3].as<std::string>();
-            mi.userLogin = messageHistoryRow.value()[i][8].as<std::string>();
+            mi.userLogin = logins.value()[mi.senderID - 1][2].as<std::string>();
             result.emplace_back(mi);
         }
     }
