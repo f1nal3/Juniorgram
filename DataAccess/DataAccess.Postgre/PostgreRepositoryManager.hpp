b67ff57333fe9dfa02149db8992902f5e2a49519
vDrWool <vdrwool@gmail.com>
2021-10-20 14:42:51 +0300

    Add documentation

--- DataAccess/DataAccess.Postgre/PostgreRepositoryManager.hpp
+++ DataAccess/DataAccess.Postgre/PostgreRepositoryManager.hpp
@@ -20,13 +20,23 @@ namespace DataAccess
 	using MethodReference = TReturn (TIRepository::*)(TArgs...);
 	
 	using RequestTask = std::packaged_task<std::any(void)>;
+	using RawFuture   = std::future<std::any>;
 
+	/* @brief  fmt function uses for converting arguments,
+	*          that you pass to repository method to correct from.
+	*  @param  ref - argument that is passing to reposiroty method.
+	*  @return Return correct format argument. 
+			   (For fundamental types like int, float, etc - by value, all other - by const ref).
+	*/
 	template <typename Type>
 	std::conditional_t<std::is_fundamental_v<Type>, std::remove_reference_t<Type>, const Type&> fmt(const Type& ref)
 	{
 		return ref;
 	}
 
+	/* @brief   Priority enum.
+	*  @details The more priority value the higher priority.
+	*/
 	enum class ePriority : std::uint8_t
 	{
 		_1,  _2,  _3,  _4,  _5,
@@ -34,6 +44,10 @@ namespace DataAccess
 		_11, _12, _13, _14, _15
 	};
 
+	/* @brief   RepositoryRequest object uses for storing request in one container.
+	*		    It is just a wrapper for std::packaged_task.
+	*  @details It is also stores a priority of this request.
+	*/
 	struct RepositoryRequest
 	{
 	private:
@@ -60,12 +74,18 @@ namespace DataAccess
 
 	public:
 
+		/* @brief  Getter for priority from request.
+		*  @return ePriority.
+		*/
 		ePriority getPriority(void) const noexcept
 		{
 			return mPriority;
 		}
 
-		std::future<std::any> getFutureFromTask(void)
+		/* @brief  Getter for raw future from task.
+		*  @return future<any>.
+		*/
+		RawFuture getFutureFromTask(void)
 		{
 			return mTask.get_future();
 		}
@@ -77,30 +97,42 @@ namespace DataAccess
 		bool operator == (const RepositoryRequest& task) const noexcept { return this->mPriority == task.mPriority; }
 		bool operator != (const RepositoryRequest& task) const noexcept { return this->mPriority != task.mPriority; }
 
+		/* @brief Invoke operator for request task. 
+		*/
 		inline void operator()(void) 
 		{
 			mTask();
 		}
 	};
 
+	/* @brief   Custom future.
+	*  @details Uses like common wrapper for std::future.
+	*/
 	template <typename TFromAny>
 	struct FutureResult
 	{
 	private:
 
-		std::future<std::any> mFuture;
+		RawFuture mFuture;
 
 	public:
 
-		FutureResult(std::future<std::any>&& future)
+		FutureResult(RawFuture&& future)
 			: mFuture(std::move(future)) {}
 
-		TFromAny get()
+		/* @brief   Like common std::future::get().
+		*  @details The difference is that returns not a raw data (any).
+		*  @return  Already known type value.
+		*/
+		TFromAny get(void)
 		{
 			return std::any_cast<TFromAny>(mFuture.get());
 		}
 	};
 
+	/* @brief   PostgreRepositoryManager controls handler for repository requests.
+	*  @details Controls push to queue and furthur processing of requests (have own thread for it).
+	*/
 	class PostgreReposiotoryManager
 	{
         private:
@@ -130,6 +162,13 @@ namespace DataAccess
 
 		public:
 
+			/* @brief   Exists for add methods in the queue.
+			*  @warning Pass all arguments through 'fmt()' function!!!
+			*  @param   (Template arg) priority - priority of request.
+			*  @param   MethodReference - just a pointer to member function needed to add to the queue.
+			*  @param   args - list of args that passing to member function.
+			*  @return  Return future object from repository member function.
+			*/
 			template <ePriority priority = ePriority::_15, typename TIRepository, typename TReturn, typename... TArgs>
             FutureResult<TReturn> pushRequest(const MethodReference<TIRepository, TReturn, TArgs...>& methodRef, TArgs&&... args)
 			{
@@ -147,6 +186,9 @@ namespace DataAccess
 				return futureResult;
 			}
 
+			/* @brief  Predicat for checking: is queue empty?
+			*  @return if empty - true, else - false.
+			*/
 			bool empty(void) const noexcept 
 			{ 
 				return mQueue.empty();
@@ -155,6 +197,9 @@ namespace DataAccess
 			// TODO: Do better handler 
 			// (Probably, will be better to create thread pool 
 			// and use it for server and manager).
+			/* @brief   Requests handler.
+			*  @details Creates inside itself a thread that handle all requsts in queue. 
+			*/
 			void handleRequests(void) 
 			{ 
 				mReposritoryRequestsHandler = std::thread(
@@ -171,6 +216,8 @@ namespace DataAccess
                 );
 			}
 
+			/* @brief Stopping handler.
+			*/
 			void stopHandler() 
 			{ 
 				mHandlerState = false;
@@ -194,6 +241,9 @@ namespace DataAccess
 				return RepositoryRequest(priority, task);
 			}
 
+			/* @brief  Extract requst from queue.
+			*  @return Repository request.
+			*/
 			RepositoryRequest privatePopRequest(void) noexcept
             {
                 std::unique_lock<std::mutex> lck(mPopMutex);
@@ -204,6 +254,8 @@ namespace DataAccess
                 return request;
             }
 
+			/* @brief Needs to register all repositories in repository container.
+			*/
 			void privateRegisterRepositories(void)
             {
                 mRepositories->registerRepository<IChannelsRepository, ChannelsRepository>();
