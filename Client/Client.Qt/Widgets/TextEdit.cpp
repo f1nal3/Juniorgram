019b5c1c1c5ffd72b642e1f1d68d53ed08272a0d
Sirzhatina <45270865+Sirzhatina@users.noreply.github.com>
2021-11-11 18:15:27 +0300

    Renamed signal. Functor instead of signals and slots

--- Client/Client.Qt/Widgets/TextEdit.cpp
+++ Client/Client.Qt/Widgets/TextEdit.cpp
@@ -33,8 +33,8 @@ TextEdit::TextEdit(QWidget* parent) : QWidget(parent), _settings(Settings::getIn
     _italicButton->setClickCallback([&]() { styleButtonClick(_italicSymbolOpen, _italicSymbolClose); });
     _underlineButton->setClickCallback([&]() { styleButtonClick(_underlineSymbolOpen, _underlineSymbolClose); });
     _sendButton->setClickCallback([&]() { sendButtonClick(); });
-    connect(_messageInput.get(), &FlatTextEdit::textChanged, this, &TextEdit::textChanged);
-    connect(_messageInput.get(), SIGNAL(messageSent()), this, SLOT(sendButtonClick()));
+    connect(_messageInput.get(), &FlatTextEdit::textChanged,          this, &TextEdit::textChanged);
+    connect(_messageInput.get(), &FlatTextEdit::shiftAndEnterPressed, this, &sendButtonClick);
     setMaximumHeight(Style::valueDPIScale(400));
     setMinimumHeight(Style::valueDPIScale(100));
 }
