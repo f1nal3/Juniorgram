690629c3e5553f09788667882f23eb638dc2736c
Sergey Mazurkevich <45270865+Sirzhatina@users.noreply.github.com>
2022-01-13 17:45:24 +0300

    reorganized try-catch blocks in Server (#471)
    * reorganized try-catch blocks in Server
    
    * The content of main has partly been taken out of try block
    
    * removed try-catch from Server::start()
    
    * Server::start() is void from now on

--- Server/Server.Core/Server.Core.Static/Server.cpp
+++ Server/Server.Core/Server.Core.Static/Server.cpp
@@ -375,34 +375,20 @@ Server::Server(const uint16_t& port)
 
 Server::~Server() { stop(); }
 
-bool Server::start()
+void Server::start()
 {
-    try
+    waitForClientConnection();
+
+    size_t threadsCount = std::thread::hardware_concurrency();
+    threadsCount > 1 ? --threadsCount : threadsCount = 1;
+    
+    for (size_t i = 0; i < threadsCount; ++i)
     {
-        waitForClientConnection();
-
-        size_t threadsCount = std::thread::hardware_concurrency();
-        threadsCount > 1 ? --threadsCount : threadsCount = 1;
-
-        for (size_t i = 0; i < threadsCount; ++i)
-        {
-            mThreads.emplace_back(std::thread([this]() { mContext.run(); }));
-        }
-
-        mPostgreManager->handleRequests();
-
-        Base::Logger::FileLogger::getInstance().log("[SERVER] Started!", Base::Logger::LogLevel::INFO);
-        return true;
+        mThreads.emplace_back(std::thread([this]() { mContext.run(); }));
     }
-    catch (std::exception& exception)
-    {
-        Base::Logger::FileLogger::getInstance().log
-        (
-            std::string("[SERVER] Exception: ") + exception.what(),
-            Base::Logger::LogLevel::ERR
-        );
-        return false;
-    }
+    mPostgreManager->handleRequests();
+
+    Base::Logger::FileLogger::getInstance().log("[SERVER] Started!", Base::Logger::LogLevel::INFO);
 }
 
 void Server::stop()
