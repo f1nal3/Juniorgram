3ddd379f8ced797e677a1e77092bdfc7721f1443
memorisecodead <e33lement.kotoff@gmail.com>
2022-03-07 13:39:05 +0300

    Replacing exception output with a FileLogger

--- Client/Client.Core/Client.Core.Static/Client.cpp
+++ Client/Client.Core/Client.Core.Static/Client.cpp
@@ -27,7 +27,8 @@ bool Client::connectToServer(const std::string_view& host, const uint16_t port)
     }
     else
     {
-        Base::Logger::FileLogger::getInstance().log("[CLIENT] Started!", Base::Logger::LogLevel::INFO);
+        Base::Logger::FileLogger::getInstance().log
+        ("[CLIENT] Started!", Base::Logger::LogLevel::INFO);
     }
 
     asio::ip::tcp::resolver resolver(_context);
@@ -37,7 +38,6 @@ bool Client::connectToServer(const std::string_view& host, const uint16_t port)
 
     try
     {
-        /// Auto = asio::ip::tcp::resolver::results_type
         auto endpoints = resolver.resolve(host, std::to_string(port));
         _connection->connectToServer(endpoints);
         _contextThread = std::thread([=]() {
@@ -47,7 +47,7 @@ bool Client::connectToServer(const std::string_view& host, const uint16_t port)
             }
             _serverAccept = false;
             onDisconnect();
-        });
+            });
     }
     catch (std::exception& exception)
     {
@@ -62,6 +62,9 @@ void Client::disconnectFromServer()
     if (isConnected())
     {
         _connection->disconnect();
+
+        Base::Logger::FileLogger::getInstance().log
+        ("[CLIENT] Disconnect from Server", Base::Logger::LogLevel::INFO);
     }
 
     _context.stop();
@@ -101,6 +104,7 @@ void Client::pingServer() const
 
     auto timeNow = std::chrono::system_clock::now();
     timeNow      = message.mHeader.mTimestamp;
+
     send(message);
 }
 
@@ -123,9 +127,8 @@ void Client::deleteChannel(const std::string channelName) const
     Network::Message networkMessage;
     networkMessage.mHeader.mMessageType = MessageType::ChannelDeleteRequest;
 
-    std::string ri;
-    ri                   = channelName;
-    networkMessage.mBody = std::make_any<std::string>(ri);
+    std::string _channelName = channelName;
+    networkMessage.mBody     = std::make_any<std::string>(_channelName);
     send(networkMessage);
 }
 
@@ -152,11 +155,11 @@ void Client::storeMessage(const std::string& message, const uint64_t channelID) 
     Network::Message networkMessage;
     networkMessage.mHeader.mMessageType = MessageType::MessageStoreRequest;
 
-    Network::MessageInfo mi;
-    mi.message   = message;
-    mi.channelID = channelID;
+    Network::MessageInfo _messageInfo;
+    _messageInfo.message = message;
+    _messageInfo.channelID = channelID;
 
-    networkMessage.mBody = std::make_any<Network::MessageInfo>(mi);
+    networkMessage.mBody = std::make_any<Network::MessageInfo>(_messageInfo);
     send(networkMessage);
 }
 
@@ -165,12 +168,12 @@ void Client::storeReply(const std::string& message, uint64_t channelID, uint64_t
     Network::Message networkMessage;
     networkMessage.mHeader.mMessageType = MessageType::ReplyStoreRequest;
 
-    Network::ReplyInfo ri;
-    ri.message   = message;
-    ri.channelID = channelID;
-    ri.msgID     = msgID;
+    Network::ReplyInfo _replyInfo;
+    _replyInfo.message = message;
+    _replyInfo.channelID = channelID;
+    _replyInfo.msgID     = msgID;
 
-    networkMessage.mBody = std::make_any<Network::ReplyInfo>(ri);
+    networkMessage.mBody = std::make_any<Network::ReplyInfo>(_replyInfo);
     send(networkMessage);
 }
 
@@ -197,6 +200,9 @@ void Client::userAuthorization(const std::string& login, const std::string& pass
     message.mHeader.mMessageType = MessageType::LoginRequest;
     message.mBody                = std::make_any<LoginInfo>(loginInfo);
 
+    Base::Logger::FileLogger::getInstance().log
+    ("User " + loginInfo.login + " logged in.", Base::Logger::LogLevel::INFO);
+
     send(message);
 }
 
@@ -209,23 +215,25 @@ void Client::messageAll() const
 
 void Client::userMessageDelete(const uint64_t messageID) const
 {
-    Network::MessageInfo mi;
-    mi.msgID = messageID;
+    Network::MessageInfo _messageInfo;
+    _messageInfo.msgID = messageID;
 
     Network::Message message;
     message.mHeader.mMessageType = MessageType::MessageDeleteRequest;
-    message.mBody                = std::make_any<Network::MessageInfo>(mi);
+    message.mBody                = std::make_any<Network::MessageInfo>(_messageInfo);
+
     send(message);
 }
 
 void Client::userMessageDelete(const std::string& messageText) const
 {
-    Network::MessageInfo mi;
-    mi.message = messageText;
+    Network::MessageInfo _messageInfo;
+    _messageInfo.message = messageText;
 
     Network::Message message;
     message.mHeader.mMessageType = MessageType::MessageDeleteRequest;
-    message.mBody                = std::make_any<Network::MessageInfo>(mi);
+    message.mBody                = std::make_any<Network::MessageInfo>(_messageInfo);
+
     send(message);
 }
 
@@ -234,9 +242,10 @@ void Client::subscriptionChannel(const std::uint64_t channelID) const
     Network::Message networkMessage;
     networkMessage.mHeader.mMessageType = MessageType::ChannelSubscribeRequest;
 
-    Network::ChannelSubscriptionInfo ri;
-    ri.channelID         = channelID;
-    networkMessage.mBody = std::make_any<Network::ChannelSubscriptionInfo>(ri);
+    Network::ChannelSubscriptionInfo _channelInfo;
+    _channelInfo.channelID = channelID;
+    networkMessage.mBody   = std::make_any<Network::ChannelSubscriptionInfo>(_channelInfo);
+
     send(networkMessage);
 }
 
@@ -245,9 +254,10 @@ void Client::leaveChannel(const std::string channelName) const
     Network::Message networkMessage;
     networkMessage.mHeader.mMessageType = MessageType::ChannelLeaveRequest;
 
-    std::string ri;
-    ri                   = channelName;
-    networkMessage.mBody = std::make_any<std::string>(ri);
+    std::string _channelName = channelName;
+    
+    networkMessage.mBody = std::make_any<std::string>(channelName);
+
     send(networkMessage);
 }
 
@@ -256,9 +266,10 @@ void Client::createChannel(const std::string channelName) const
     Network::Message networkMessage;
     networkMessage.mHeader.mMessageType = MessageType::ChannelCreateRequest;
 
-    std::string ri;
-    ri                   = channelName;
-    networkMessage.mBody = std::make_any<std::string>(ri);
+    std::string _channelName = channelName;
+   
+    networkMessage.mBody     = std::make_any<std::string>(_channelName);
+
     send(networkMessage);
 }
 
@@ -268,20 +279,20 @@ void Client::createDirectChat(uint64_t receiverId) const
     networkMessage.mHeader.mMessageType = MessageType::DirectMessageCreateRequest;
 
     networkMessage.mBody = std::make_any<uint64_t>(receiverId);
+
     send(networkMessage);
 }
 
 void Client::userMessageReaction(const std::uint64_t messageID, const std::uint32_t reactionID) const
 {
-    Network::MessageInfo mi;
-    mi.msgID = messageID;
+    Network::MessageInfo _messageInfo;
+    _messageInfo.msgID = messageID;
 
-    // using max uint32_t as special value
-    mi.reactions[reactionID] = std::numeric_limits<std::uint32_t>::max();
+    _messageInfo.reactions[reactionID] = std::numeric_limits<std::uint32_t>::max();
     
     Network::Message message;
     message.mHeader.mMessageType = MessageType::MessageReactionRequest;
-    message.mBody                = std::make_any<Network::MessageInfo>(mi);
+    message.mBody                = std::make_any<Network::MessageInfo>(_messageInfo);
 
     send(message);
 }
@@ -291,7 +302,7 @@ void Client::loop()
     while (!_incomingMessagesQueue.empty())
     {
         const Message message = _incomingMessagesQueue.pop_front();
-        auto          _realtime   = Utility::getTimeNow();
+        auto         _timeNow = Utility::getTimeNow();
 
         std::cout << "[" << Utility::getTimeNow() << "]\n";
 
@@ -322,7 +333,6 @@ void Client::loop()
 
             case MessageType::ServerMessage:
             {
-                // T\todo add handling
                 uint64_t clientID = 0;
                 onServerMessage(clientID);
             }
@@ -423,7 +433,11 @@ void Client::loop()
             break;
 
             default:
-                std::cerr << "[Client][Warning] unimplemented[" << uint32_t(message.mHeader.mMessageType) << "]\n";
+                Base::Logger::FileLogger::getInstance().log
+                (
+                    "[CLIENT][WARNING] unimplemented: " + uint32_t(message.mHeader.mMessageType),
+                    Base::Logger::LogLevel::ERR
+                );
         }
     }
 }
@@ -431,57 +445,91 @@ void Client::loop()
 void Client::onLoginAnswer(bool success)
 {
     (void)(success);
-    std::cerr << "[Client][Warning] login answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log(
+        "[CLIENT][WARNING] login answer is not implemented: ",
+        Base::Logger::LogLevel::ERR
+    );
 }
 
-void Client::onServerAccepted() { std::cerr << "[Client][Warning] server accepted is not implemented\n"; }
+void Client::onServerAccepted() 
+{ 
+     Base::Logger::FileLogger::getInstance().log(
+         "[CLIENT][WARNING] server accepted is not implemented: ", 
+         Base::Logger::LogLevel::ERR
+     );
+}
 
 void Client::onServerPing(double timestamp)
 {
     (void)(timestamp);
-    std::cerr << "[Client][Warning] server ping is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] server ping is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
+
 
 void Client::onServerMessage(const uint64_t clientId)
 {
     (void)(clientId);
-    std::cerr << "[Client][Warning] server message is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] server message is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onChannelListRequest(const std::vector<Network::ChannelInfo>& channels)
 {
     (void)(channels);
-    std::cerr << "[Client][Warning] channel list request is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] channel list request is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onMessageHistoryAnswer(const std::vector<Network::MessageInfo>& messages)
 {
     (void)(messages);
-    std::cerr << "[Client][Warning] message history answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] message history answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onMessageStoreAnswer(Utility::StoringMessageCodes storingMessageCode)
 {
     (void)(storingMessageCode);
-    std::cerr << "[Client][Warning] message store answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] message store answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onRegistrationAnswer(Utility::RegistrationCodes registrationCode)
 {
     (void)(registrationCode);
-    std::cerr << "[Client][Warning] registration answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] registration answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onUserMessageDeleteAnswer(const Utility::DeletingMessageCodes deletingState)
 {
     (void)(deletingState);
-    std::cerr << "[Client][Warning] onUserMessageDeleteAnswer answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] onUserMessageDeleteAnswer answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onMessageReactionAnswer(const Utility::ReactionMessageCodes reactionState)
 {
     (void)(reactionState);
-    std::cerr << "[Client][Warning] onMessageReaction answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] onMessageReaction answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onDisconnect() { std::cerr << "[Client][Warning] onDisconnect is not implemented\n"; }
@@ -489,55 +537,82 @@ void Client::onDisconnect() { std::cerr << "[Client][Warning] onDisconnect is no
 void Client::onMessageSendFailed(const Message& message) const
 {
     (void)(message);
-    std::cerr << "[Client][Warning] onMessageSendFailed is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] onMessageSendFailed is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onReplyHistoryAnswer(const std::vector<Network::ReplyInfo>& replies)
 {
     (void)(replies);
-    std::cerr << "[Client][Warning] reply answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] reply answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onReplyStoreAnswer(Utility::StoringReplyCodes storingReplyCode)
 {
     (void)(storingReplyCode);
-    std::cerr << "[Client][Warning] reply store answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] reply store answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onChannelLeaveAnswer(Utility::ChannelLeaveCodes ChannelLeaveCode)
 {
     (void)(ChannelLeaveCode);
-    std::cerr << "[Client][Warning] leave channel answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] leave channel answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onChannelSubscribingAnswer(Utility::ChannelSubscribingCodes subscribingChannelCode)
 {
     (void)(subscribingChannelCode);
-    std::cerr << "[Client][Warning] subscribing channel is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] subscribing channel is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onChannelSubscribingListAnswer(std::vector<uint64_t> subscribingChannelList)
 {
     (void)(subscribingChannelList);
-    std::cerr << "[Client][Warning] subscribing channel list is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] subscribing channel list is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onChannelDeleteAnswer(Utility::ChannelDeleteCode channelDeleteCode)
 {
     (void)(channelDeleteCode);
-    std::cerr << "[Client][Warning] leave channel answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] leave channel answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onChannelCreateAnswer(Utility::ChannelCreateCodes channelCreateCode)
 {
     (void)(channelCreateCode);
-    std::cerr << "[Client][Warning] create channel answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] create channel answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 void Client::onDirectMessageCreateAnswer(Utility::DirectMessageStatus directMessageCreateAnswer)
 {
     (void)(directMessageCreateAnswer);
-    std::cerr << "[Client][Warning] direct message create answer is not implemented\n";
+    Base::Logger::FileLogger::getInstance().log
+    (
+        "[CLIENT][WARNING] direct message create answer is not implemented: ", Base::Logger::LogLevel::ERR
+    );
 }
 
 }  // namespace Network
