5e2a7bc2b3289273d2ce9f9808b08f5921ddc983
f1nal3 <Valentine.Timoshik@gmail.com>
2022-04-01 14:24:31 +0300

    Hash check removed

--- cmake/ConanModule.cmake
+++ cmake/ConanModule.cmake
@@ -8,7 +8,6 @@ if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
   message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
   file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.0/conan.cmake"
                 "${CMAKE_BINARY_DIR}/conan.cmake"
-                EXPECTED_HASH SHA256=396e16d0f5eabdc6a14afddbcfff62a54a7ee75c6da23f32f7a31bc85db23484
                 TLS_VERIFY ON)
 endif()
 
