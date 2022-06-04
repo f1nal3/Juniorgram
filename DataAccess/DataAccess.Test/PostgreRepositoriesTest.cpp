37225c24692b65bb4972f0945dd2c3137b2bae0d
ivanboika <deadman2002xxx@gmail.com>
2022-06-04 20:45:17 +0300

    register cover

--- DataAccess/DataAccess.Test/PostgreRepositoriesTest.cpp
+++ DataAccess/DataAccess.Test/PostgreRepositoriesTest.cpp
@@ -13,20 +13,37 @@ using DataAccess::PostgreTable;
 
 TEST_CASE("PostgreRepositories test", "[dummy]")
 {
-	RegisterRepository testRepos(PostgreAdapter::Instance());
-
 	auto testEmail{ "anotheruser@gmail.com" };
 	auto testLogin{ "anotheruser" };
 	auto testPassHash{ "65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5" };
 
 	SECTION("Register repository")
 	{
-		SECTION("Register user") {
+		RegisterRepository testRegisterRepos(PostgreAdapter::Instance());
 
+		SECTION("Register user") 
+		{
 			Network::RegistrationInfo ourUser(testEmail, testLogin, testPassHash);
-			REQUIRE_NOTHROW(testRepos.registerUser(ourUser));
+			REQUIRE(testRegisterRepos.registerUser(ourUser) == Utility::RegistrationCodes::SUCCESS);
+		}
+		SECTION("Trying to register new user with data of already existing user")
+		{
+			auto newTestLogin{ "newuser" };
+			Network::RegistrationInfo badUserEmail(testEmail, newTestLogin, testPassHash);
+			REQUIRE(testRegisterRepos.registerUser(badUserEmail) == Utility::RegistrationCodes::EMAIL_ALREADY_EXISTS);
+
+			auto newTestEmail{ "newuser@gmail.com" };
+			Network::RegistrationInfo badUserLogin(newTestEmail, testLogin, testPassHash);
+			REQUIRE(testRegisterRepos.registerUser(badUserLogin) == Utility::RegistrationCodes::LOGIN_ALREADY_EXISTS);
 		}
 	}
+
+	SECTION("Login repository")
+	{
+		LoginRepository testLoginRepos(PostgreAdapter::Instance());
+
+	}
+
 	SECTION("Delete user")
 	{
 		auto testAdapter = PostgreAdapter::Instance();
