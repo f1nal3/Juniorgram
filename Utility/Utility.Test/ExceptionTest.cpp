3dcb7de4399a45e86e5e04d00d470b76b45ab9bb
Viachaslau Lepeshau <slavex001@tut.by>
2022-06-13 20:42:42 +0300

    Additional coverage "Exception" file

--- Utility/Utility.Test/ExceptionTest.cpp
+++ Utility/Utility.Test/ExceptionTest.cpp
@@ -8,17 +8,40 @@ using Catch::Contains;
     throw Utility::NotImplementedException("Not implemented", __FILE__, __LINE__);
 }
 
+[[noreturn]] static void NoExceptionMock() {
+    Utility::NotImplementedException("Implemented", __FILE__, __LINE__);
+}
+
 [[noreturn]] static void OperationDBExceptionMock() { 
     throw Utility::OperationDBException("DB error operation", __FILE__, __LINE__); 
 }
 
+[[noreturn]] static void OperationDBNoExceptionMock() {
+    Utility::OperationDBException("DB success operation", __FILE__, __LINE__);
+}
+
 TEST_CASE("Test NotImplementedException format", "[Utility.Exception]")
 {
-    REQUIRE_THROWS_WITH(ExceptionThrowerMock(), Contains("Not implemented") && Contains("ExceptionTest.cpp"));
+    SECTION("Exception is expected")
+    {
+        REQUIRE_THROWS_WITH(ExceptionThrowerMock(), Contains("Not implemented") && Contains("ExceptionTest.cpp"));
+    }
 
+    SECTION("Exception is not expected")
+    {
+        REQUIRE_NOTHROW(NoExceptionMock());
+    }
 }
 
 TEST_CASE("Test OperationDBException format", "[Utility.Exception]")
 {
-    REQUIRE_THROWS_WITH(OperationDBExceptionMock(), Contains("DB error operation") && Contains("ExceptionTest.cpp"));
+    SECTION("Exception is expected")
+    {
+        REQUIRE_THROWS_WITH(OperationDBExceptionMock(), Contains("DB error operation") && Contains("ExceptionTest.cpp"));
+    }
+
+    SECTION("Exception is not expected")
+    {
+        REQUIRE_NOTHROW(OperationDBNoExceptionMock());
+    }
 }
