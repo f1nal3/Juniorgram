f7cf6a8cb00fc2fa6aa8ec6c9c8cbaea507a9f5d
ivanboika <deadman2002xxx@gmail.com>
2022-06-23 23:36:16 +0300

    Addition cover of QueryBuilder

--- DataAccess/DataAccess.Test/QueryBuilderTest.cpp
+++ DataAccess/DataAccess.Test/QueryBuilderTest.cpp
@@ -10,7 +10,7 @@ using DataAccess::IAdapter;
 TEST_CASE("QueryBuilder cover", "[dummy]")
 {
 	QueryBuilder<pqxx::result> testQueryBuilder(Utility::DatabaseType::DB_POSTGRE, "users",
-								IAdapter::getInstance<DataAccess::PostgreAdapter>(DBOptions::real));
+								IAdapter::getInstance<DataAccess::PostgreAdapter>(DBOptions::test));
 
 	SECTION("Try to initialize the reference and catch the exception")
 	{
@@ -22,4 +22,15 @@ TEST_CASE("QueryBuilder cover", "[dummy]")
 		REQUIRE_THROWS(testQueryBuilder.Delete());
 		REQUIRE_NOTHROW(testQueryBuilder.changeTable("users"));
 	}
+
+	SECTION("Cover of 'join' method")
+	{
+		auto badJoinType{ 25 };
+
+		REQUIRE_NOTHROW(testQueryBuilder.Select()->join(Utility::SQLJoinType::J_FULL, "channels", "id"));
+		REQUIRE_NOTHROW(testQueryBuilder.Select()->join(Utility::SQLJoinType::J_LEFT, "channels", "id"));
+		REQUIRE_NOTHROW(testQueryBuilder.Select()->join(Utility::SQLJoinType::J_RIGHT, "channels", "id"));
+		REQUIRE_THROWS(testQueryBuilder.Select()->join(std::any_cast<Utility::SQLJoinType>(badJoinType), "users", "login"));
+	}
 }
+ 
