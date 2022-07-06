d28e69b804aa02ddce0b2fc6ebe77ad58c6e407e
KliKonNik <Brayan1023@gmail.com>
2022-07-06 18:54:05 +0300

    Task 514: Fixed some code smells.

--- DataAccess/DataAccess.Postgre/PostgreRepositories.hpp
+++ DataAccess/DataAccess.Postgre/PostgreRepositories.hpp
@@ -16,13 +16,13 @@ struct ChannelsRepository final : IChannelsRepository, AbstractPostgreRepository
 {
     explicit ChannelsRepository(const std::shared_ptr<IAdapter>& adapter) { _pTable = std::make_unique<PostgreTable>("users", adapter); }
     
-    std::vector<Models::ChannelInfo>       getAllChannelsList() final;
-    Utility::ChannelLeaveCodes             leaveChannel(const Models::ChannelLeaveInfo& channel) final;
-    Utility::ChannelSubscribingCodes       subscribeToChannel(const Models::ChannelSubscriptionInfo& channel) final;
-    std::vector<uint64_t>                  getChannelSubscriptionList(uint64_t userID) final;
+    std::vector<Models::ChannelInfo>       getAllChannelsList() override;
+    Utility::ChannelLeaveCodes             leaveChannel(const Models::ChannelLeaveInfo& channel) override;
+    Utility::ChannelSubscribingCodes       subscribeToChannel(const Models::ChannelSubscriptionInfo& channel) override;
+    std::vector<uint64_t>                  getChannelSubscriptionList(uint64_t userID) override;
 
-    Utility::ChannelDeleteCode  deleteChannel(const Models::ChannelDeleteInfo& channel) final;
-    Utility::ChannelCreateCodes createChannel(const Models::ChannelInfo& channel) final;
+    Utility::ChannelDeleteCode  deleteChannel(const Models::ChannelDeleteInfo& channel) override;
+    Utility::ChannelCreateCodes createChannel(const Models::ChannelInfo& channel) override;
 
     ~ChannelsRepository() override = default;
 };
@@ -41,7 +41,7 @@ struct LoginRepository : ILoginRepository, AbstractPostgreRepository
 {
     explicit LoginRepository(const std::shared_ptr<IAdapter>& adapter) { _pTable = std::make_unique<PostgreTable>("users", adapter); }
 
-    std::uint64_t loginUser(const Models::LoginInfo& loginInfo) final;
+    std::uint64_t loginUser(const Models::LoginInfo& loginInfo) override;
 
     ~LoginRepository() = default;
 };
@@ -50,11 +50,11 @@ struct MessagesRepository final : IMessagesRepository, AbstractPostgreRepository
 {
     explicit MessagesRepository(const std::shared_ptr<IAdapter>& adapter) { _pTable = std::make_unique<PostgreTable>("users", adapter); }
 
-    std::vector<Models::MessageInfo>       getMessageHistory(const std::uint64_t channelID) final;
-    Utility::StoringMessageCodes           storeMessage(const Models::MessageInfo& mi) final;
-    Utility::DeletingMessageCodes          deleteMessage(const Models::MessageInfo& mi) final;
-    Utility::EditingMessageCodes           editMessage(const Models::MessageInfo& mi) final;
-    Utility::ReactionMessageCodes          updateMessageReactions(const Models::MessageInfo& mi) final;
+    std::vector<Models::MessageInfo>       getMessageHistory(const std::uint64_t channelID) override;
+    Utility::StoringMessageCodes           storeMessage(const Models::MessageInfo& mi) override;
+    Utility::DeletingMessageCodes          deleteMessage(const Models::MessageInfo& mi) override;
+    Utility::EditingMessageCodes           editMessage(const Models::MessageInfo& mi) override;
+    Utility::ReactionMessageCodes          updateMessageReactions(const Models::MessageInfo& mi) override;
 
     ~MessagesRepository() override = default;
 
@@ -68,7 +68,7 @@ struct RegisterRepository final : IRegisterRepository, AbstractPostgreRepository
 {
     explicit RegisterRepository(const std::shared_ptr<IAdapter>& adapter) { _pTable = std::make_unique<PostgreTable>("users", adapter); }
 
-    Utility::RegistrationCodes registerUser(const Models::RegistrationInfo& ri) final;
+    Utility::RegistrationCodes registerUser(const Models::RegistrationInfo& ri) override;
 
     ~RegisterRepository() override = default;
 };
@@ -78,8 +78,8 @@ struct RepliesRepository final : IRepliesRepository, AbstractPostgreRepository
 {
     explicit RepliesRepository(const std::shared_ptr<IAdapter>& adapter) { _pTable = std::make_unique<PostgreTable>("msgs", adapter); }
 
-    std::vector<Models::ReplyInfo>       getReplyHistory(const std::uint64_t channelID) final;
-    Utility::StoringReplyCodes           storeReply(const Models::ReplyInfo& rsi) final;
+    std::vector<Models::ReplyInfo>       getReplyHistory(const std::uint64_t channelID) override;
+    Utility::StoringReplyCodes           storeReply(const Models::ReplyInfo& rsi) override;
 
     ~RepliesRepository() override = default;
 
