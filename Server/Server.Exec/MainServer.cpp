ef7415e8c0848e87edd477f3a156f48d82b8a0db
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2022-07-11 16:21:30 +0300

    Corrected the main function to start the server

--- Server/Server.Exec/MainServer.cpp
+++ Server/Server.Exec/MainServer.cpp
@@ -1,17 +1,27 @@
-#include <ArgParser.hpp>
-#include <Server.hpp>
+
 #include <iostream>
 #include <stdexcept>
+
+#include "ServerBuilder.hpp"
+#include "ArgParser.hpp"
+
+using Server::Builder::ServerBuilder;
+
+// "dbname=juniorgram user=postgres hostaddr=127.0.0.1 port=5432"
+// "hostaddr=127.0.0.1 port=5432 dbname=testdb user=postgres"
 
 int main(int argc, const char** argv)
 {
     ArgParser parser(argc, argv);
-    Server::Server server(ArgParser::getPort());
-    server.start();
+    auto server = ServerBuilder()
+                                .setValue(parser.getPair<int>("--port"))
+                                .setValue("--db_config", "hostaddr=127.0.0.1 port=5432 dbname=juniorgram user=postgres password=1111")
+                                .make();
+    server->start();
 
     while (true)
     {
-        server.update();
+        server->update();
     }
     return 0;
 }
