9ee168b97ddc86207b6fcd52ca67574f9a106ec1
Stanislau Mazur <e33lement.kotoff@gmail.com>
2022-08-09 17:29:45 +0300

    Removing one / & adding spaces for documentation

--- DataAccess/DataAccess.Postgre/PostgreAdapter.hpp
+++ DataAccess/DataAccess.Postgre/PostgreAdapter.hpp
@@ -87,4 +87,4 @@ public:
     void closeConnection(void) override;
 };
 
-} /// namespace DataAccess
+} // namespace DataAccess
--- DataAccess/Public/Include/DataAccess/QueryBuilder.hpp
+++ DataAccess/Public/Include/DataAccess/QueryBuilder.hpp
@@ -64,6 +64,7 @@ public:
 
         return dynamic_cast<SQLSelect<ResultType>*>(_statement);
     }
+
     /**
      * @brief SQL insert query.
      * @return SQLInsert pointer.
@@ -85,6 +86,7 @@ public:
 
         return dynamic_cast<SQLInsert<ResultType>*>(_statement);
     }
+
     /**
      * @brief SQL update query.
      * @return SQLUpdate pointer.
@@ -106,6 +108,7 @@ public:
 
         return dynamic_cast<SQLUpdate<ResultType>*>(_statement);
     }
+
     /**
      * @brief SQL delete query.
      * @return SQLDelete pointer.
@@ -142,6 +145,7 @@ public:
 
         _tableName = newTableName;
     }
+
     /**
      * @brief Changing table.
      * @param newTableName - new name of the table.
@@ -165,4 +169,4 @@ public:
         _statement = nullptr;
     }
 };
-}  /// namespace DataAccess
+}  // namespace DataAccess
