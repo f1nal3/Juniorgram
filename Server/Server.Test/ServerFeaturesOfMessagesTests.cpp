ae7ef54ac538e924eac8c5abcd71a47a736ed665
Stanislau Mazur <e33lement.kotoff@gmail.com>
2022-08-24 16:32:47 +0300

    Test

---
+++ Server/Server.Test/ServerFeaturesOfMessagesTests.cpp
@@ -0,0 +1,153 @@
+#include <catch2/catch.hpp>
+#include <TestUtility.hpp>
+
+#include <thread>
+
+using namespace TestUtility;
+
+TEST_CASE("Check message history request of server")
+{
+    testServer serverTest(ServerInfo::Port::test);
+
+    std::thread threadServer([&serverTest]() {
+        serverTest.start();
+        testServerUpdating(serverTest);
+    });
+
+    Client Client;
+
+    std::thread threadMockClient([&Client]() {
+        Client.connectToServer(ServerInfo::Address::remote, ServerInfo::Port::test);
+        CHECK_NOTHROW(testSendingMessages(Client, MessageType::MessageHistoryRequest));
+        Client.disconnectFromServer();
+    });
+
+    threadMockClient.join();
+    threadServer.join();
+}
+
+TEST_CASE("Check direct message create request from server side")
+{
+    testServer serverTest(ServerInfo::Port::test);
+
+    std::thread threadServer([&serverTest]() {
+        serverTest.start();
+        testServerUpdating(serverTest);
+    });
+
+    Client Client;
+
+    std::thread threadMockClient([&Client]() {
+        Client.connectToServer(ServerInfo::Address::local, ServerInfo::Port::test);
+        CHECK_NOTHROW(testSendingMessages(Client, MessageType::DirectMessageCreateRequest));
+        Client.disconnectFromServer();
+    });
+
+    threadMockClient.join();
+    threadServer.join();
+}
+
+TEST_CASE("Check all messages from server side")
+{
+    testServer serverTest(ServerInfo::Port::test);
+
+    std::thread threadServer([&serverTest]() {
+        serverTest.start();
+        testServerUpdating(serverTest);
+    });
+
+    Client Client;
+
+    std::thread threadMockClient([&Client]() {
+        Client.connectToServer(ServerInfo::Address::local, ServerInfo::Port::test);
+        CHECK_NOTHROW(testSendingMessages(Client, MessageType::MessageAll));
+        Client.disconnectFromServer();
+    });
+
+    threadMockClient.join();
+    threadServer.join();
+}
+
+TEST_CASE("Check message store request from server side")
+{
+    testServer serverTest(ServerInfo::Port::test);
+
+    std::thread threadServer([&serverTest]() {
+        serverTest.start();
+        testServerUpdating(serverTest);
+    });
+
+    Client Client;
+
+    std::thread threadMockClient([&Client]() {
+        Client.connectToServer(ServerInfo::Address::local, ServerInfo::Port::test);
+        CHECK_NOTHROW(testSendingMessages(Client, MessageType::MessageStoreRequest));
+        Client.disconnectFromServer();
+    });
+
+    threadMockClient.join();
+    threadServer.join();
+}
+
+TEST_CASE("Check message reaction request from server side")
+{
+    testServer serverTest(ServerInfo::Port::test);
+
+    std::thread threadServer([&serverTest]() {
+        serverTest.start();
+        testServerUpdating(serverTest);
+    });
+
+    Client Client;
+
+    std::thread threadMockClient([&Client]() {
+        Client.connectToServer(ServerInfo::Address::local, ServerInfo::Port::test);
+        CHECK_NOTHROW(testSendingMessages(Client, MessageType::MessageReactionRequest));
+        Client.disconnectFromServer();
+    });
+
+    threadMockClient.join();
+    threadServer.join();
+}
+
+TEST_CASE("Check message edit from server side")
+{
+    testServer serverTest(ServerInfo::Port::test);
+
+    std::thread threadServer([&serverTest]() {
+        serverTest.start();
+        testServerUpdating(serverTest);
+    });
+
+    Client Client;
+
+    std::thread threadMockClient([&Client]() {
+        Client.connectToServer(ServerInfo::Address::local, ServerInfo::Port::test);
+        CHECK_NOTHROW(testSendingMessages(Client, MessageType::MessageEditRequest));
+        Client.disconnectFromServer();
+    });
+
+    threadMockClient.join();
+    threadServer.join();
+}
+
+TEST_CASE("Check message delete request from server side")
+{
+    testServer serverTest(ServerInfo::Port::test);
+
+    std::thread threadServer([&serverTest]() {
+        serverTest.start();
+        testServerUpdating(serverTest);
+    });
+
+    Client Client;
+
+    std::thread threadMockClient([&Client]() {
+        Client.connectToServer(ServerInfo::Address::local, ServerInfo::Port::test);
+        CHECK_NOTHROW(testSendingMessages(Client, MessageType::MessageDeleteRequest));
+        Client.disconnectFromServer();
+    });
+
+    threadMockClient.join();
+    threadServer.join();
+}
---
+++ Server/Server.Test/ServerWorkflowTests.cpp
@@ -0,0 +1,61 @@
+#include <catch2/catch.hpp>
+#include <Client.hpp>
+#include <ServerInfo.hpp>
+#include <Server.hpp>
+
+#include <thread>
+
+using Client           = Network::Client;
+using Message          = Network::Message;
+using testServer       = Server::Server;
+using MessageType      = Message::MessageType;
+
+TEST_CASE("Workflow startup server")
+{
+    testServer serverTest(ServerInfo::Port::test);
+
+    std::thread threadServer([&serverTest]() {
+        CHECK_NOTHROW(serverTest.start());
+        REQUIRE_NOTHROW(serverTest.stop());
+    });
+
+    Client Client;
+
+    std::thread threadMockClient([&Client]() {
+        CHECK_NOTHROW(Client.connectToServer(ServerInfo::Address::local, ServerInfo::Port::test));
+        REQUIRE_NOTHROW(Client.disconnectFromServer());
+    });
+
+    threadMockClient.join();
+    threadServer.join();
+}
+
+suppressWarning(4244, "-Wconversion")
+suppressWarning(4242, "-Wconversion")
+TEST_CASE("Workflow fail startup server")
+{    
+    uint32_t       testBadPort = 6666666;
+    testServer     badServer(testBadPort);   
+
+    CHECK_NOTHROW(badServer.start()); 
+}
+restoreWarning
+restoreWarning
+
+TEST_CASE("Check disconnect from client")
+{
+    testServer serverTest(ServerInfo::Port::test);
+
+    REQUIRE_NOTHROW(serverTest.start());
+
+    asio::io_context            testContext;
+    Utility::SafeQueue<Message> testIncomingMessagesQueue;
+
+    CHECK_NOTHROW(serverTest.messageClient
+    (
+    std::shared_ptr<Network::Connection>(std::make_shared<Network::Connection>
+    (
+        Network::Connection::OwnerType::CLIENT, testContext,
+        asio::ip::tcp::socket(testContext), testIncomingMessagesQueue)), Message()
+    ));
+}
