02cdb8c0ed55b623ee08f0995996c3d09c1402c0
Stanislau Mazur <e33lement.kotoff@gmail.com>
2022-09-03 01:42:45 +0300

    Removing useless names

--- Server/Server.Test/MockObjects/Objects/MockClient.hpp
+++ Server/Server.Test/MockObjects/Objects/MockClient.hpp
@@ -16,14 +16,8 @@ using asio::io_context;
 using asio::ip::tcp;
 using Network::Connection;
 using Network::Client;
-//using Models::LoginInfo;
 using Network::Message;
-//using Models::MessageInfo;
-//using Models::RegistrationInfo;
-//using Models::ReplyInfo;
 using Utility::SafeQueue;
-//using RTC = std::chrono::system_clock;
-//using UtilityTime::timestamp_t;
 
 /**
 *  @class MockClient.
@@ -34,116 +28,108 @@ class MockClient : public Client
 {
 protected:
     /**
-     * @brief Method for signaling of disconnect from server.
-     */
+    * @brief Method for signaling of disconnect from server.
+    */
     void onDisconnect();
 
     /**
-     * @brief Method for signaling of sending message to server.
-     */
+    * @brief Method for signaling of sending message to server.
+    */
     void onMessageSendFailed();
 
     /**
-     * @brief Method for signaling of login answer.
-     */
+    * @brief Method for signaling of login answer.
+    */
     void onLoginAnswer(bool success);
 
     /**
-     * @brief Method for signaling of server accepting.
-     */
+    * @brief Method for signaling of server accepting.
+    */
     void onServerAccepted();
 
     /**
-     * @brief Method for signaling of server ping.
-     */
+    * @brief Method for signaling of server ping.
+    */
     void onServerPing(double timestamp);
 
     /**
-     * @brief Method for signaling of server message.
-     */
+    * @brief Method for signaling of server message.
+    */
     void onServerMessage(const uint64_t clientID);
 
     /**
-     * @brief Method for signaling of channel list request.
-     */
+    * @brief Method for signaling of channel list request.
+    */
     void onChannelListRequest(const std::vector<Models::ChannelInfo>& channels);
 
     /**
-     * @brief Method for signaling of message history answer.
-     */
+    * @brief Method for signaling of message history answer.
+    */
     void onMessageHistoryAnswer(const std::vector<Models::MessageInfo>& messages);
 
     /**
-     * @brief Method for signaling of message store answer.
-     */
+    * @brief Method for signaling of message store answer.
+    */
     void onMessageStoreAnswer(Utility::StoringMessageCodes storingMessageCode);
 
     /**
-     * @brief Method for signaling of message delete answer.
-     */
+    * @brief Method for signaling of message delete answer.
+    */
     void onUserMessageDeleteAnswer(const Utility::DeletingMessageCodes deletingState);
 
     /**
-     * @brief Method for signaling of registration answer.
-     */
+    * @brief Method for signaling of registration answer.
+    */
     void onRegistrationAnswer(Utility::RegistrationCodes registrationCode);
 
     /**
-     * @brief Method for signaling of reply history answer.
-     */
+    * @brief Method for signaling of reply history answer.
+    */
     void onReplyHistoryAnswer(Utility::StoringReplyCodes storingReplyCode);
 
     /**
-     * @brief Method for signaling of reply store answer.
-     */
+    * @brief Method for signaling of reply store answer.
+    */
     void onReplyStoreAnswer(Utility::StoringReplyCodes storingReplyCode);
 
     /**
-     * @brief Method for signaling of channel leave answer.
-     */
+    * @brief Method for signaling of channel leave answer.
+    */
     void onChannelLeaveAnswer(Utility::ChannelLeaveCodes channelLeaveCode);
 
     /**
-     * @brief Method for signaling of channel subscription answer.
-     */
+    * @brief Method for signaling of channel subscription answer.
+    */
     void onChannelSubscribingAnswer(const Utility::ChannelSubscribingCodes subscribingChannelCode);
 
     /**
-     * @brief Method for signaling of channel subscription list answer.
-     */
+    * @brief Method for signaling of channel subscription list answer.
+    */
     void onChannelSubscribingListAnswer(const std::vector<uint64_t>& subscribingChannelList);
 
     /**
-     * @brief Method for signaling of channel delete answer.
-     */
+    * @brief Method for signaling of channel delete answer.
+    */
     void onChannelDeleteAnswer(Utility::ChannelDeleteCode channelDeleteCode);
 
     /**
-     * @brief Method for signaling of channel create answer.
-     */
+    * @brief Method for signaling of channel create answer.
+    */
     void onChannelCreateAnswer(Utility::ChannelCreateCodes channelCreateCode);
 
     /**
-     * @brief Method for signaling of direct message create answer.
-     */
+    * @brief Method for signaling of direct message create answer.
+    */
     void onDirectMessageCreateAnswer(Utility::DirectMessageStatus directMessageCreateAnswer);
 
     /**
-     * @brief Method for signaling of reaction answer.
-     */
+    * @brief Method for signaling of reaction answer.
+    */
     void onMessageReactionAnswer(Utility::ReactionMessageCodes reactionState);
 
     /**
-     * @brief Method for signaling of edit message answer.
-     */
+    * @brief Method for signaling of edit message answer.
+    */
     void onEditMessageAnswer(Utility::EditingMessageCodes reactionState);
-
-private:
-    io_context                  testContext;
-    std::thread                 testContextThread;
-    std::unique_ptr<Connection> testConnection;
-    SafeQueue<Message>          testIncomingMessagesQueue;
-
-    bool serverAccept = false;
 };
 }  // namespace MockClient
