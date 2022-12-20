ef5dad277d870b9b2c7ea1d6419881add4027621
Stanislau Mazur <e33lement.kotoff@gmail.com>
2022-12-20 23:40:10 +0300

    time commit

--- Server/Server.Test/ServerChannelMessagesTests.cpp
+++ Server/Server.Test/ServerChannelMessagesTests.cpp
@@ -1,98 +1,98 @@
-#include <catch2/catch.hpp>
-#include "TestUtility.hpp"
-
-using namespace TestUtility;
-
-/*
-TEST_CASE("Check channel create request of server")
-{
-    Client     Client;
-    testServer serverTest(ServerInfo::Port::test);
-    bool       acceptingConnection = true;
-      
-    serverTest.start();
-
-    if (bindOfConnectToServer(Client) == acceptingConnection)
-    {
-        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelCreateRequest));
-    }
-    testServerUpdating(serverTest);
-}
-*/
-
-TEST_CASE("Check channel list request from server side")
-{
-    Client     Client;
-    testServer serverTest(ServerInfo::Port::test);
-    bool       acceptingConnection = true;
-
-    serverTest.start();
-
-    if (bindOfConnectToServer(Client) == acceptingConnection)
-    {
-        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelListRequest));
-    }
-    testServerUpdating(serverTest);
-}
-
-TEST_CASE("Check channel subscribe request from server side")
-{
-    Client     Client;
-    testServer serverTest(ServerInfo::Port::test);
-    bool       acceptingConnection = true;
-
-    serverTest.start();
-
-    if (bindOfConnectToServer(Client) == acceptingConnection)
-    {
-        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelSubscribeRequest));
-    }
-    testServerUpdating(serverTest);
-}
-
-/*
-TEST_CASE("Check channel subscription list request from server side")
-{
-    Client     Client;
-    testServer serverTest(ServerInfo::Port::test);
-    bool       acceptingConnection = true;
-
-    serverTest.start();
-
-    if (bindOfConnectToServer(Client) == acceptingConnection)
-    {
-        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelSubscriptionListRequest));
-    }
-    testServerUpdating(serverTest);
-}
-*/
-
-TEST_CASE("Check channel leave request from server side")
-{
-    Client     Client;
-    testServer serverTest(ServerInfo::Port::test);
-    bool       acceptingConnection = true;
-
-    serverTest.start();
-
-    if (bindOfConnectToServer(Client) == acceptingConnection)
-    {
-        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelLeaveRequest));
-    }
-    testServerUpdating(serverTest);
-}
-
-TEST_CASE("Check channel delete request from server side")
-{
-    Client     Client;
-    testServer serverTest(ServerInfo::Port::test);
-    bool       acceptingConnection = true;
-
-    serverTest.start();
-
-    if (bindOfConnectToServer(Client) == acceptingConnection)
-    {
-        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelDeleteRequest));
-    }
-    testServerUpdating(serverTest);
-}
+//#include <catch2/catch.hpp>
+//#include "TestUtility.hpp"
+//
+//using namespace TestUtility;
+//
+///*
+//TEST_CASE("Check channel create request of server")
+//{
+//    Client     Client;
+//    testServer serverTest(ServerInfo::Port::test);
+//    bool       acceptingConnection = true;
+//      
+//    serverTest.start();
+//
+//    if (bindOfConnectToServer(Client) == acceptingConnection)
+//    {
+//        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelCreateRequest));
+//    }
+//    testServerUpdating(serverTest);
+//}
+//*/
+//
+//TEST_CASE("Check channel list request from server side")
+//{
+//    Client     Client;
+//    testServer serverTest(ServerInfo::Port::test);
+//    bool       acceptingConnection = true;
+//
+//    serverTest.start();
+//
+//    if (bindOfConnectToServer(Client) == acceptingConnection)
+//    {
+//        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelListRequest));
+//    }
+//    testServerUpdating(serverTest);
+//}
+//
+//TEST_CASE("Check channel subscribe request from server side")
+//{
+//    Client     Client;
+//    testServer serverTest(ServerInfo::Port::test);
+//    bool       acceptingConnection = true;
+//
+//    serverTest.start();
+//
+//    if (bindOfConnectToServer(Client) == acceptingConnection)
+//    {
+//        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelSubscribeRequest));
+//    }
+//    testServerUpdating(serverTest);
+//}
+//
+///*
+//TEST_CASE("Check channel subscription list request from server side")
+//{
+//    Client     Client;
+//    testServer serverTest(ServerInfo::Port::test);
+//    bool       acceptingConnection = true;
+//
+//    serverTest.start();
+//
+//    if (bindOfConnectToServer(Client) == acceptingConnection)
+//    {
+//        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelSubscriptionListRequest));
+//    }
+//    testServerUpdating(serverTest);
+//}
+//*/
+//
+//TEST_CASE("Check channel leave request from server side")
+//{
+//    Client     Client;
+//    testServer serverTest(ServerInfo::Port::test);
+//    bool       acceptingConnection = true;
+//
+//    serverTest.start();
+//
+//    if (bindOfConnectToServer(Client) == acceptingConnection)
+//    {
+//        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelLeaveRequest));
+//    }
+//    testServerUpdating(serverTest);
+//}
+//
+//TEST_CASE("Check channel delete request from server side")
+//{
+//    Client     Client;
+//    testServer serverTest(ServerInfo::Port::test);
+//    bool       acceptingConnection = true;
+//
+//    serverTest.start();
+//
+//    if (bindOfConnectToServer(Client) == acceptingConnection)
+//    {
+//        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ChannelDeleteRequest));
+//    }
+//    testServerUpdating(serverTest);
+//}
--- Server/Server.Test/ServerDefaultMessagesTests.cpp
+++ Server/Server.Test/ServerDefaultMessagesTests.cpp
@@ -1,36 +1,36 @@
-#include <catch2/catch.hpp>
-#include "TestUtility.hpp"
-
-using namespace TestUtility;
-
-TEST_CASE("Check server ping")
-{
-    Client     Client;
-    testServer serverTest(ServerInfo::Port::test);
-    bool       acceptingConnection = true;
-
-    serverTest.start();
-
-    if (bindOfConnectToServer(Client) == acceptingConnection)
-    {
-        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ServerPing));
-    }
-    testServerUpdating(serverTest);
-}
-
-TEST_CASE("Check default request of server side")
-{
-    Client     Client;
-    testServer serverTest(ServerInfo::Port::test);
-
-    constexpr auto failedType         = int16_t(666);
-    bool          acceptingConnection = true;
-
-    serverTest.start();
-
-    if (bindOfConnectToServer(Client) == acceptingConnection)
-    {
-        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType(failedType)));
-    }
-    testServerUpdating(serverTest);
-}
+//#include <catch2/catch.hpp>
+//#include "TestUtility.hpp"
+//
+//using namespace TestUtility;
+//
+//TEST_CASE("Check server ping")
+//{
+//    Client     Client;
+//    testServer serverTest(ServerInfo::Port::test);
+//    bool       acceptingConnection = true;
+//
+//    serverTest.start();
+//
+//    if (bindOfConnectToServer(Client) == acceptingConnection)
+//    {
+//        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType::ServerPing));
+//    }
+//    testServerUpdating(serverTest);
+//}
+//
+//TEST_CASE("Check default request of server side")
+//{
+//    Client     Client;
+//    testServer serverTest(ServerInfo::Port::test);
+//
+//    constexpr auto failedType         = int16_t(666);
+//    bool          acceptingConnection = true;
+//
+//    serverTest.start();
+//
+//    if (bindOfConnectToServer(Client) == acceptingConnection)
+//    {
+//        CHECK_NOTHROW(bindOfSendingMessage(Client, MessageType(failedType)));
+//    }
+//    testServerUpdating(serverTest);
+//}
