85551713c28c7fafb0a5bbff2534951202bc43ff
Stanislau Mazur <e33lement.kotoff@gmail.com>
2023-02-09 17:15:48 +0300

    Adding bad method of make message

--- Server/Server.Test/TestUtility.hpp
+++ Server/Server.Test/TestUtility.hpp
@@ -49,6 +49,12 @@ using ChannelInfo             = Models::ChannelInfo;
 
 constexpr bool testAcceptingConnection = true;
 
+enum TypeOfMessageBody
+{
+    PoorBody,
+    StrongBody
+};
+
 /**
 * @brief struct ConfigArguments.
 * @details Designed to receive arguments from the ServerBuilder.
@@ -214,8 +220,186 @@ inline Message& makeMessage(Message& message, MessageType messageType) noexcept
 
             case Message::MessageType::ChannelListRequest:
             {
-                std::vector<Models::ChannelInfo> testChannelList;
-                message.mBody = std::make_any<std::vector<Models::ChannelInfo>>(testChannelList);
+                message.mBody = std::make_any<std::vector<Models::ChannelInfo>>(mesgFiller.getChannelInfo()._channelID);
+
+                break;
+            }
+            case Message::MessageType::ServerAccept:
+            {
+                Base::Logger::FileLogger::getInstance().log
+                (
+                    "[TestUtilityMessage]: Server accepted the connection!",
+                     Base::Logger::LogLevel::INFO
+                );
+
+                break;
+            }
+            default:
+            {
+                Base::Logger::FileLogger::getInstance().log
+                (
+                    "[TestUtilityMessage]: Unimplemented[" + std::to_string
+                    (uint32_t(message.mHeader.mMessageType)) + "]",
+                     Base::Logger::LogLevel::WARNING
+                );
+                throw std::invalid_argument("Unimplemented!");
+
+                break;
+            }
+        }
+    }
+    catch (const std::invalid_argument& testException)
+    {
+        Base::Logger::FileLogger::getInstance().log
+        (
+            testException.what(),
+            Base::Logger::LogLevel::WARNING
+        );
+    }
+    return message;
+}
+
+inline Message& makePoorMessage(Message& message, MessageType messageType) noexcept
+{
+    message.mHeader.mMessageType = messageType;
+    message.mHeader.mTimestamp   = RTC::to_time_t(RTC::now());
+
+    try
+    {
+        switch (messageType)
+        {
+            case Message::MessageType::RegistrationRequest:
+            {
+                RegistrationInfo registrationInfo;
+                registrationInfo._login = "";
+                message.mBody = std::make_any<RegistrationInfo>(registrationInfo);
+
+                break;
+            }
+
+            case Message::MessageType::LoginRequest:
+            {
+                LoginInfo loginInfo;
+                loginInfo._login = "";
+                message.mBody = std::make_any<LoginInfo>(loginInfo);
+
+                break;
+            }
+
+            case Message::MessageType::ChannelCreateRequest:
+            {
+                std::string_view channelInfo = "";
+                message.mBody                = std::make_any<std::string>(channelInfo);
+
+                break;
+            }
+
+            case Message::MessageType::ChannelSubscriptionListRequest:
+            {
+                Models::ChannelSubscriptionInfo channelInfo;
+                channelInfo._channelID = 0;
+                channelInfo._userID    = 0;
+                message.mBody          = std::make_any<Models::ChannelSubscriptionInfo>(channelInfo);
+
+                break;
+            }
+
+            case Message::MessageType::ChannelSubscribeRequest:
+            {
+                Models::ChannelSubscriptionInfo messageInfo;
+                messageInfo._channelID = 0;
+                messageInfo._userID    = 0;
+                message.mBody = std::any_cast<Models::ChannelSubscriptionInfo>(messageInfo);
+
+                break;
+            }
+
+            case Message::MessageType::ChannelLeaveRequest:
+            {
+                std::string_view messageInfo = "";
+                message.mBody                = std::make_any<std::string>(messageInfo);
+
+                break;
+            }
+
+            case Message::MessageType::ReplyHistoryRequest:
+            {
+                message.mBody = std::make_any<uint64_t>(0);
+
+                break;
+            }
+
+            case Message::MessageType::ReplyStoreRequest:
+            {
+                ReplyInfo replyInfo;
+                replyInfo._channelID = 0;
+                message.mBody = std::make_any<ReplyInfo>(replyInfo);
+
+                break;
+            }
+
+            case Message::MessageType::MessageReactionRequest:
+            {
+                MessageInfo messageInfo;
+                messageInfo._msgID = 0;
+                message.mBody = std::make_any<MessageInfo>(messageInfo);
+
+                break;
+            }
+
+            case Message::MessageType::DirectMessageCreateRequest:
+            {
+                message.mBody = std::make_any<uint64_t>(0);
+
+                break;
+            }
+
+            case Message::MessageType::MessageHistoryRequest:
+            {
+                message.mBody = std::make_any<uint64_t>(0);
+
+                break;
+            }
+
+            case Message::MessageType::MessageStoreRequest:
+            {
+                MessageInfo messageInfo;
+                messageInfo._channelID = 0;
+                message.mBody = std::make_any<MessageInfo>(messageInfo);
+
+                break;
+            }
+
+            case Message::MessageType::MessageEditRequest:
+            {
+                MessageInfo messageInfo;
+                messageInfo._msgID = 0;
+                message.mBody = std::make_any<MessageInfo>(messageInfo);
+
+                break;
+            }
+
+            case Message::MessageType::MessageDeleteRequest:
+            {
+                MessageInfo messageInfo;
+                messageInfo._msgID = 0;
+                message.mBody = std::make_any<MessageInfo>(messageInfo);
+
+                break;
+            }
+
+            case Message::MessageType::ChannelDeleteRequest:
+            {
+                Models::ChannelDeleteInfo channelDeleteInfo;
+                channelDeleteInfo._channelID = 0;
+                message.mBody = std::make_any<std::string>(channelDeleteInfo._channelName);
+
+                break;
+            }
+
+            case Message::MessageType::ChannelListRequest:
+            {
+                message.mBody = std::make_any<std::vector<Models::ChannelInfo>>(0);
 
                 break;
             }
@@ -260,15 +444,24 @@ inline Message& makeMessage(Message& message, MessageType messageType) noexcept
 *          After the message is generated, it is sent to the client side for further processing. /
 * @param const Client& Client(Client object), const MessageType mesgType(type of message).
 */
-inline void testSendingMessages(const Client& Client, const MessageType mesgType) noexcept
+inline void testSendingMessages(const Client& Client, const MessageType mesgType, const TypeOfMessageBody mesgBody) noexcept
 {
     Message    message;
     std::mutex scopedMutex;
 
     std::scoped_lock scopedLock(scopedMutex);
-    makeMessage(message, mesgType);
-    std::this_thread::sleep_for(milliseconds(1000));
-    Client.send(message);
+    if (mesgBody == TypeOfMessageBody::PoorBody)
+    {
+        makePoorMessage(message, mesgType);
+        std::this_thread::sleep_for(milliseconds(1000));
+        Client.send(message);
+    }
+    else
+    {
+        makeMessage(message, mesgType);
+        std::this_thread::sleep_for(milliseconds(1000));
+        Client.send(message);
+    }
 }
 
 /**
@@ -304,15 +497,24 @@ constexpr TestServer& testServerUpdating(TestServer& server) noexcept
 *          It simplifies the code and makes it compact. 
 * @param Client& Client(Client object), MessageType mesgType(type of message).
 */
-inline decltype(auto) bindOfSendingMessage(Client& Client, MessageType mesgType) noexcept
+inline decltype(auto) bindOfSendingMessage(Client& Client, MessageType mesgType, const TypeOfMessageBody mesgBody) noexcept
 {   
     using namespace std::placeholders;
+    if (mesgBody == TypeOfMessageBody::PoorBody)
+    {
+        const auto sendingBadMessage = std::bind
+        (
+            &testSendingMessages, _1,_2,_3
+        );
+        return sendingBadMessage(Client, mesgType, mesgBody);
+    }
+
     const auto sendingMessage = std::bind
     (
-        &testSendingMessages, _1, _2
+        &testSendingMessages, _1, _2,_3
     );
 
-    return sendingMessage(Client, mesgType);
+    return sendingMessage(Client, mesgType, mesgBody);
 }
 
 /**
