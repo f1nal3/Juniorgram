fcb261a1adeeec9a6433f1921ce63b84000ea0ae
Stanislau Mazur <e33lement.kotoff@gmail.com>
2023-02-09 17:13:31 +0300

    Adding MockQueryBuilder

---
+++ MockObjects/Objects/MockObjectsFiles/MockQueryBuilder.hpp
@@ -0,0 +1,44 @@
+#pragma once 
+
+#include <iostream>
+#include <string>
+
+#include <DataAccess/IAdapter.hpp>
+#include "MockDatabaseOptions.hpp"
+
+namespace MockQueryBuilder
+{
+using DataAccess::IAdapter;
+
+template <typename QueryType = TestUtility::MockRepositoryData>
+class MockQueryBuilder
+{
+public:
+    MockQueryBuilder(const std::string& type, const std::string& tableName, std::shared_ptr<IAdapter> adapter)
+        : _databaseType{type}, _tableName{tableName}, _adapter{adapter}
+    {
+    }
+
+    virtual ~MockQueryBuilder() = default;
+
+    MockQueryBuilder() = delete;
+
+    MockQueryBuilder(const MockQueryBuilder&) = delete;
+    MockQueryBuilder(MockQueryBuilder&&)      = delete;
+
+    MockQueryBuilder& operator=(const MockQueryBuilder&) = delete;
+    MockQueryBuilder& operator=(MockQueryBuilder&&)      = delete;
+
+    std::optional<std::any> SelectAndPushData(const std::string_view& repoName, const std::string_view& repoInfo)
+    {
+        return _adapter->query(repoName)->emplace<std::string>(repoInfo);
+    }
+
+    [[nodiscard]] std::shared_ptr<IAdapter>& getAdapter() noexcept { return _adapter; }
+
+private:
+    std::shared_ptr<IAdapter> _adapter;
+    std::string               _tableName;
+    std::string               _databaseType;
+};
+}  // namespace MockQueryBuilder
