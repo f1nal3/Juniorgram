839c7f278924df2d0d88e0363dc01d905d674eb2
Stanislau Mazur <e33lement.kotoff@gmail.com>
2023-02-22 02:53:53 +0300

    2stylish

--- Client/Client.Test/TestClient.hpp
+++ Client/Client.Test/TestClient.hpp
@@ -17,12 +17,32 @@ using Network::Connection;
 class TestClient
 {
 public:
+    /**
+    * @brief Connect to server with IP(host) and Port(port).
+    * @param const std::string_view& host - for identifying and accepting ip address, \
+    * @param const uint16_t port - for accepting & identifying of port.
+    */
     bool connectToServer(const std::string_view& host, const uint16_t port);
     
+    /**
+    * @brief Disconnect from server.
+    * @details This method stop connection to remote host. \
+    *   Also, it reset all context and jobs that related with TestClient. \
+    *   This method call in destructor, so in general way it is not used.
+    */ 
     void disconnectFromServer();
 
+    /**
+    * @brief Sends an initialized header message to the server.
+    * @details You can see the message types in the Message header file.
+    */ 
     void send(const Message& message) const;
-    
+
+    /**
+    * @brief TestClient virtual destructor.
+    * @details The virtual destructor calls the disconnectFromServer method, \
+    *    which checks the connection to the server. 
+    */
     virtual ~TestClient() noexcept;
 
 protected:
@@ -132,9 +152,32 @@ protected:
     void onEditMessageAnswer(Utility::EditingMessageCodes reactionState) const;
 
 private:
+    /**
+    * @brief Noose that handle incoming messages.
+    * @details Handler function to process the server response message \
+    *   for the subsequent response from the client.
+    *   (You can see the message types in the Message header file).
+    */ 
     void               noose();
+
+    /**
+    * @brief Checking the server connection.
+    * @details Returns true if connection is established.
+    */ 
     [[nodiscard]] bool isConnected() const;
+
+    /**
+    * @brief Method checks the connection to the server. 
+    * @defails Until the connection is established, \
+    *          no further work will be done.
+    */
     bool               checkServerAcception();
+
+    /**
+    * @brief Method checks if the received arguments to connect to the server are correct.
+    * @param const std::string_view& host - for identifying and accepting ip address, \
+    * @param const uint16_t port - for accepting & identifying of port.
+    */
     bool               checkConnectionArguments(const std::string_view& host, const uint16_t port) const;
 
     bool                                 _serverAccept = false;
