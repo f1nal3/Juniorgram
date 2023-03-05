8222a7ccb1a1c4af873b3c845b3e568f795e186b
Stanislau Mazur <e33lement.kotoff@gmail.com>
2023-03-05 15:48:14 +0300

    WaitFor stylish

--- Server/Server.Test/Utilities/WaitFor.hpp
+++ Server/Server.Test/Utilities/WaitFor.hpp
@@ -2,30 +2,42 @@
 
 #include <chrono>
 
-template<typename TTime = std::chrono::milliseconds>
+/**
+* @brief class waiting for a certain amount of time.
+* @details Emulates the "normal behavior" of client-server interaction. /
+*          Used in testing to temporarily process actions on the client/server side. /
+*          It is a template designed to work with different types of time, for example: seconds, milliseconds... /
+*          By default, the template works with milliseconds.
+*/
+template<typename TimeType = std::chrono::milliseconds>
 class WaitFor
 {
 public:
-    WaitFor(TTime timeForWait) :
+    explicit WaitFor(TimeType timeForWait) :
         waitTime(timeForWait) {}
 
-    ~WaitFor() { waitTime = TTime(0); }
+    WaitFor(WaitFor& other)                   = delete;
+    WaitFor& operator=(const WaitFor& other)  = delete;
+    WaitFor(WaitFor&& other) noexcept         = delete;
+    WaitFor const& operator=(WaitFor&& other) = delete;
+
+    ~WaitFor() { waitTime = TimeType(0); }
 
     void wait()
     {
-        bool sleep = true;
-        while (sleep)
+        bool IsSleep = true;
+        while (IsSleep)
         {
-            auto now     = std::chrono::system_clock::now();
-            auto elapsed = std::chrono::duration_cast<TTime>(now - start);
-            if (elapsed.count() > waitTime.count())
+            auto currentTime = std::chrono::system_clock::now();
+            auto elapsedTime = std::chrono::duration_cast<TimeType>(currentTime - startTime);
+            if (elapsedTime.count() > waitTime.count())
             {
-                sleep = false;
+                IsSleep = false;
             }
         }
     }
 
 private:
-    TTime waitTime;
-    std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+    TimeType                              waitTime;
+    std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
 };
