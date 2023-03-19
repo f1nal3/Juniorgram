794d4409a8ac800aee1a02cd34d91a7ad02a69ad
Stanislau Mazur <e33lement.kotoff@gmail.com>
2023-03-19 03:25:40 +0300

    Upgrade ServerWorkflowTests

--- Server/Server.Test/ServerWorkflowTests.cpp
+++ Server/Server.Test/ServerWorkflowTests.cpp
@@ -6,15 +6,42 @@ using TestUtility::MessageBody;
 
 TEST_CASE("Successfull Server start up [Server][Success]")
 {
-    auto testServer = makeTestServer();
+    ServerBuilder builder;
+    TestServer    testServer = builder
+                                .setValue(configArgs.getServerPortArguments())
+                                .setValue(configArgs.getDatabaseArguments())
+                                .setValue(configArgs.getHostAddrArguments())
+                                .setValue(configArgs.getDatabasePortArguments())
+                                .setValue(configArgs.getDatabaseUserArguments())
+                                .setValue(configArgs.getDatabasePasswordArguments())
+                                .setValue(getTestDatabase().release())
+                                .makeServer();
+
+    SECTION("Comparison of the specified port with the expected port")
+    {
+        REQUIRE(std::stoi(configArgs.getServerPortArguments().second) 
+            == builder.getServerPortValue());
+    }
     CHECK_NOTHROW(testServer->start());
-    testServer->stop();
 }
 
-TEST_CASE("Successfull Server start up with bad port [Server][Success]")
+TEST_CASE("Server start up with bad port [Server][Failed]")
 {   
-    // clarification: The server functionality does not provide a connection to the port with a limited value.
-    auto testServer = makeBadTestServer();
+    ServerBuilder builder;
+    TestServer    testServer = builder
+                                .setValue(configArgs.getBadServerPortArguments())
+                                .setValue(configArgs.getDatabaseArguments())
+                                .setValue(configArgs.getHostAddrArguments())
+                                .setValue(configArgs.getDatabasePortArguments())
+                                .setValue(configArgs.getDatabaseUserArguments())
+                                .setValue(configArgs.getDatabasePasswordArguments())
+                                .setValue(getTestDatabase().release())
+                                .makeServer();
+
+    SECTION("Comparison of the specified port with the expected bad port")
+    {
+        REQUIRE_FALSE(std::stoi(configArgs.getBadServerPortArguments().second) 
+            == builder.getServerPortValue());
+    }
     CHECK_NOTHROW(testServer->start());
-    testServer->stop();
 }
