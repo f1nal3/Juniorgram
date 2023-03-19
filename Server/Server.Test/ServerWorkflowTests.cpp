dd3519a60c32f03da0c1bc3817b70febc0a448ce
Stanislau Mazur <e33lement.kotoff@gmail.com>
2023-03-19 22:41:13 +0300

    upd workflow cases

--- Server/Server.Test/ServerWorkflowTests.cpp
+++ Server/Server.Test/ServerWorkflowTests.cpp
@@ -6,29 +6,19 @@ using TestUtility::MessageBody;
 
 TEST_CASE("Successfull Server start up [Server][Success]")
 {
-    ServerBuilder builder;
-    TestServer    testServer = builder
-                                .setValue(configArgs.getServerPortArguments())
-                                .setValue(configArgs.getDatabaseArguments())
-                                .setValue(configArgs.getHostAddrArguments())
-                                .setValue(configArgs.getDatabasePortArguments())
-                                .setValue(configArgs.getDatabaseUserArguments())
-                                .setValue(configArgs.getDatabasePasswordArguments())
-                                .setValue(getTestDatabase().release())
-                                .makeServer();
+    auto testServer = makeTestServer();
 
     SECTION("Comparison of the specified port with the expected port")
     {
         REQUIRE(std::stoi(configArgs.getServerPortArguments().second) 
-            == builder.getServerPortValue());
+            == TestServerInfo::Port::test);
     }
     CHECK_NOTHROW(testServer->start());
 }
 
 TEST_CASE("Server start up with bad port [Server][Failed]")
 {   
-    ServerBuilder builder;
-    TestServer    testServer = builder
+    TestServer testServer = ServerBuilder()
                                 .setValue(configArgs.getBadServerPortArguments())
                                 .setValue(configArgs.getDatabaseArguments())
                                 .setValue(configArgs.getHostAddrArguments())
@@ -41,7 +31,7 @@ TEST_CASE("Server start up with bad port [Server][Failed]")
     SECTION("Comparison of the specified port with the expected bad port")
     {
         REQUIRE_FALSE(std::stoi(configArgs.getBadServerPortArguments().second) 
-            == builder.getServerPortValue());
+            == TestServerInfo::Port::test);
     }
     CHECK_NOTHROW(testServer->start());
 }
