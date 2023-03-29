0a258722ddc56017b6915dd97adc5a0e69540444
Stanislau Mazur <70116007+memorisecodead@users.noreply.github.com>
2023-03-29 18:17:11 +0300

    Investigate a bug in the GCC Linux build.  (#644)
    * test formatting

--- Utility/Utility.Test/UtilityTimeTest.cpp
+++ Utility/Utility.Test/UtilityTimeTest.cpp
@@ -9,30 +9,19 @@ using std::chrono::system_clock;
 TEST_CASE("consoleLogTimestamp test")
 {
     std::ostringstream outputTime;
-    auto               coutBuff = std::cout.rdbuf();  // save pointer to std::cout buffer
-    std::cout.rdbuf(outputTime.rdbuf());               // substitute internal std::cout buffer with buffer of 'local' object
+    auto               coutBuff = std::cout.rdbuf();    // save pointer to std::cout buffer
+    std::cout.rdbuf(outputTime.rdbuf());    // substitute internal std::cout buffer with buffer of 'local' object
 
-    system_clock::time_point currentTime = system_clock::now();
+    system_clock::time_point bufferTime = system_clock::now();
     UtilityTime::consoleLogTimestamp();
 
     std::cout.rdbuf(coutBuff);  // go back to old buffer
 
-    std::time_t currentTime_time_t = system_clock::to_time_t(currentTime);
-    std::tm     currentTime_tm;
+    std::time_t currentTime   = system_clock::to_time_t(bufferTime);
+    std::tm     formattedTime = UtilityTime::safe_localtime(currentTime);
 
-#if defined(_MSC_VER)
-    localtime_s(&currentTime_tm, &currentTime_time_t);
-#elif defined(__unix__)
-    localtime_r(&currentTime_time_t, &currentTime_tm);
-#else
-    static std::mutex           mu;
-    std::lock_guard<std::mutex> lock(mu);
-
-    currentTime_tm = *std::localtime(&currentTime_time_t);
-#endif
-
-    std::string checkTime(25, '\0');
-    std::strftime(checkTime.data(), checkTime.size(), "%Y-%m-%d %H:%M:%S%z", &currentTime_tm);
+    std::string checkTime(20, '\0');
+    std::strftime(checkTime.data(), checkTime.size(), "%Y-%m-%d %H:%M:%S%z", &formattedTime);
 
     REQUIRE_THAT(outputTime.str(), Contains(checkTime.data()));
 }
