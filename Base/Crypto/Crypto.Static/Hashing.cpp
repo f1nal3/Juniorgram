14ff9de5bb4831b893564b6867be0f3ef189ef1b
Viachaslau Lepeshau <slavex001@tut.by>
2023-04-07 13:44:15 +0300

    Code styling

--- Base/Crypto/Crypto.Static/Hashing.cpp
+++ Base/Crypto/Crypto.Static/Hashing.cpp
@@ -1,20 +1,24 @@
-#include "Hashing.hpp"
-
 #include <cryptopp/sha.h>
 #include <cryptopp/hex.h>
 #include <cryptopp/filters.h>
 
+#include "Hashing.hpp"
+
 namespace Base::Hashing
 {
-    using namespace CryptoPP;
+    using CryptoPP::SHA256;
+    using CryptoPP::HashFilter;
+    using CryptoPP::HexEncoder;
+    using CryptoPP::StringSink;
+    using CryptoPP::StringSource;
 
-std::string Base::Hashing::SHA_256(const std::string& plainText, const std::string& componentForSalt)
-{
-    SHA256       hash;
-    std::string  digest;
-    auto         stream = new HashFilter(hash, new HexEncoder(new StringSink(digest)));
-    StringSource strSrc(componentForSalt + plainText, true, stream);
+    std::string Base::Hashing::SHA_256(const std::string& plainText, const std::string& componentForSalt)
+    {
+        SHA256       hash;
+        std::string  digest;
+        auto         stream = new HashFilter(hash, new HexEncoder(new StringSink(digest)));
+        StringSource strSrc(componentForSalt + plainText, true, stream);
 
-    return digest;
-}
+        return digest;
+    }
 }  // namespace Base::Hashing
