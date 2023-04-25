abd0027e3bd6bfe14c6933e136e679c1a71960d1
Viachaslau Lepeshau <slavex001@tut.by>
2023-04-25 16:59:40 +0300

    Added conversion public rsa key from/to std::string

--- Base/Models/Public/Include/Models/Primitives.hpp
+++ Base/Models/Public/Include/Models/Primitives.hpp
@@ -310,6 +310,21 @@ struct RSAKeyPair
 {
     CryptoPP::RSA::PublicKey  publicKey;
     CryptoPP::RSA::PrivateKey  privateKey;
-};
 
+    std::string getPublicKeyAsString()
+    {
+        std::string publicKeyStr;
+        CryptoPP::StringSink  stringSink(publicKeyStr);
+        publicKey.DEREncode(stringSink);
+        return publicKeyStr;
+    }
+
+    CryptoPP::RSA::PublicKey getPublicKeyFromString(const std::string& publicServerKeyStr)
+    {
+        CryptoPP::StringSource   stringSource(publicServerKeyStr, true);
+        CryptoPP::RSA::PublicKey publicServerKey;
+        publicServerKey.BERDecode(stringSource);
+        return publicServerKey;
+    }
+};
 }  // namespace Models
