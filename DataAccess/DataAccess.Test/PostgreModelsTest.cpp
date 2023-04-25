bf0ce2914bed0b0eb8628d08c2851ca0d9271f84
ivanboika <deadman2002xxx@gmail.com>
2023-04-25 13:43:10 +0300

    Added test to check 'insert' logic

--- DataAccess/DataAccess.Test/PostgreModelsTest.cpp
+++ DataAccess/DataAccess.Test/PostgreModelsTest.cpp
@@ -12,12 +12,27 @@ using DataAccess::UserInfo;
 using DataAccess::PostgreAdapter;
 using DataAccess::PGQueryBuilder;
 
-TEST_CASE("Check user registration", "Filling the table")
+TEST_CASE("Check user registration", "[dummy]")
 {
-    SECTION("We want to fill the table once")
+    auto testTable = std::make_unique<PGQueryBuilder>("users", PostgreAdapter::Instance(DBOptions::test));
+
+    SECTION("Wanna add a user", "Kinda a 'userRegistration method in repos'")
+    {
+        User testUser({"3", "aboba3@gmail.com", "aboba3", "kindahash3"});
+
+        std::tuple insertData{std::pair{testUser.fieldName(UserInfo::ID), testUser[UserInfo::ID]},
+                              std::pair{testUser.fieldName(UserInfo::EMAIL), testUser[UserInfo::EMAIL]}, 
+                              std::pair{testUser.fieldName(UserInfo::LOGIN), testUser[UserInfo::LOGIN]}, 
+                              std::pair{testUser.fieldName(UserInfo::PASSHASH), testUser[UserInfo::PASSHASH]}};
+
+        auto responce = testTable->Insert()->columns(insertData)->execute();
+
+        REQUIRE_FALSE(responce.has_value());
+    }
+
+    SECTION("We want to fill the table once", "Check how properly map fills")
     {
         User testUser;
-        auto testTable = std::make_unique<PGQueryBuilder>("users", PostgreAdapter::Instance(DBOptions::test));
 
         auto responce = testTable->Select()->columns({"*"})->Where("id = '1'")->execute();
         if (responce.has_value())
@@ -26,5 +41,9 @@ TEST_CASE("Check user registration", "Filling the table")
 
             REQUIRE(testUser[UserInfo::ID] == "1");
         }
+    }
+    SECTION("Deleting user", "Check agility of the model with our adapter")
+    {
+
     }
 }
