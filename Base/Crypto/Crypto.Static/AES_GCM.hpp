4e136e0bf6f5702cdfff7a05091c1f3140abaa07
Viachaslau Lepeshau <slavex001@tut.by>
2023-04-30 13:52:06 +0300

    Significant reworking of symmetric encryption

--- Base/Crypto/Crypto.Static/AES_GCM.hpp
+++ Base/Crypto/Crypto.Static/AES_GCM.hpp
@@ -23,51 +23,35 @@ namespace Base::Crypto::Symmetric
     using CryptoPP::HashVerificationFilter;
     using CryptoPP::AAD_CHANNEL;
     using CryptoPP::DEFAULT_CHANNEL;
+    using Base::Logger::FileLogger;
+    using Base::Logger::LogLevel;
 
 /** @class AES_GCM
 * @brief Encryption class according to the AES GCM algorithm
-* @details Uses key from SessionKeyHolder and initial vector from MessageHeader.
+* @details Uses key; uses initialization vector and authentification data from MessageHeader.
 */
 class AES_GCM final : public Base::Crypto::ICryptography
 {
 public:
-    AES_GCM(uint64_t userId_, const std::string& login_) :
-        _userID(userId_),
-        _authData(Base::Hashing::SHA_256(login_))
-    {
-    };
 
-    /** @brief Method for encrypting data
-    * @details Designed for use in EncryptionHandler
-    */
-    Utility::GeneralCodes encrypt(const std::string& initVector, yas::shared_buffer& bodyBuffer)
+    /// @brief Method for encryption
+    EncryptionState encrypt(yas::shared_buffer& bodyBuffer,
+                            const SecByteBlock& key,
+                            const SecByteBlock& initVector,
+                            const std::string& authData)
     {
         try
         {
-            yas::shared_buffer                  encryptedBody(bodyBuffer.size);
+            yas::shared_buffer                  encryptedBody(bodyBuffer.size + TAG_SIZE);
             GCM<AES, GCM_2K_Tables>::Encryption encryptor;
 
-            {
-                SecByteBlock key;
-                SecByteBlock initVec(reinterpret_cast<const byte*>(initVector.data()), initVector.size());
-
-                if (keyHolder::Instance().getOwner() == Owner::SERVER)
-                {
-                    key = keyHolder::Instance().getUserKey(_userID);
-                    encryptor.SetKeyWithIV(key.begin(), key.size(), initVec.begin(), initVec.size());
-                }
-                else
-                {
-                    key = keyHolder::Instance().getKey();
-                    encryptor.SetKeyWithIV(key.begin(), key.size(), initVec.begin(), initVec.size());
-                }
-            }
+            encryptor.SetKeyWithIV(key.data(), key.size(), initVector.data(), initVector.size());
 
             AuthenticatedEncryptionFilter authEncFilter(encryptor,
-                new ArraySink(reinterpret_cast<byte*>(encryptedBody.data.get()), encryptedBody.size),
-                false, TAG_SIZE);
+                                                        new ArraySink(reinterpret_cast<byte*>(encryptedBody.data.get()), encryptedBody.size),
+                                                        false, TAG_SIZE);
 
-            authEncFilter.ChannelPut(AAD_CHANNEL, reinterpret_cast<const byte*>(_authData.data()), _authData.size());
+            authEncFilter.ChannelPut(AAD_CHANNEL, reinterpret_cast<const byte*>(authData.data()), authData.size());
             authEncFilter.ChannelMessageEnd(AAD_CHANNEL);
 
             authEncFilter.ChannelPut(DEFAULT_CHANNEL, reinterpret_cast<const byte*>(bodyBuffer.data.get()), bodyBuffer.size);
@@ -75,35 +59,22 @@ public:
 
             bodyBuffer = encryptedBody;
         }
-        catch (CryptoPP::Exception& e)
+        catch (CryptoPP::Exception& exception)
         {
-            Base::Logger::FileLogger::getInstance().log(e.what(), Base::Logger::LogLevel::ERR);
-            return Utility::GeneralCodes::FAILED;
+            FileLogger::getInstance().log(exception.what(), LogLevel::ERR);
+            return EncryptionState::FAILURE;
         }
-        return Utility::GeneralCodes::SUCCESS;
+        return EncryptionState::SUCCESS;
     }
 
-    /** @brief Method for decrypting data
-     * @details Designed for use in EncryptionHandler
-     */
-    Utility::GeneralCodes decrypt(yas::shared_buffer& buffer, const std::string& initVector)
+    /// @brief Method for decryption
+    EncryptionState decrypt(yas::shared_buffer& buffer,
+                            const SecByteBlock& key,
+                            const SecByteBlock& initVector,
+                            const std::string& authData)
     {
         GCM<AES, GCM_2K_Tables>::Decryption decryptor;
-        {
-            SecByteBlock key;
-            SecByteBlock initVec(reinterpret_cast<const byte*>(initVector.data()), initVector.size());
-
-            if (keyHolder::Instance().getOwner() == Owner::SERVER)
-            {
-                key = keyHolder::Instance().getUserKey(_userID);
-                decryptor.SetKeyWithIV(key.begin(), key.size(), initVec.begin(), initVec.size());
-            }
-            else
-            {
-                key = keyHolder::Instance().getKey();
-                decryptor.SetKeyWithIV(key.begin(), key.size(), initVec.begin(), initVec.size());
-            }
-        }
+        decryptor.SetKeyWithIV(key.data(), key.size(), initVector.data(), initVector.size());
 
         AuthenticatedDecryptionFilter authDecrFilter(decryptor, NULL,
             AuthenticatedDecryptionFilter::Flags::MAC_AT_BEGIN |
@@ -111,19 +82,16 @@ public:
             TAG_SIZE);
 
         yas::shared_buffer encryptedBody(buffer.data.get(), buffer.size - TAG_SIZE);
-        yas::shared_buffer msgAuthCode(buffer.data.get() + buffer.size, TAG_SIZE);
+        yas::shared_buffer msgAuthCode(buffer.data.get() + buffer.size - TAG_SIZE, TAG_SIZE);
 
         if (buffer.size != encryptedBody.size + msgAuthCode.size || TAG_SIZE != msgAuthCode.size)
         {
-            using Base::Logger::FileLogger;
-            using Base::Logger::LogLevel;
-
             FileLogger::getInstance().log("Message splitting error", LogLevel::ERR);
-            return Utility::GeneralCodes::FAILED;
+            return EncryptionState::FAILURE;
         }
 
         authDecrFilter.ChannelPut(DEFAULT_CHANNEL, reinterpret_cast<const byte*>(msgAuthCode.data.get()), msgAuthCode.size);
-        authDecrFilter.ChannelPut(AAD_CHANNEL, reinterpret_cast<const byte*>(_authData.data()), _authData.size());
+        authDecrFilter.ChannelPut(AAD_CHANNEL, reinterpret_cast<const byte*>(authData.data()), authData.size());
         authDecrFilter.ChannelPut(DEFAULT_CHANNEL, reinterpret_cast<const byte*>(encryptedBody.data.get()), encryptedBody.size);
 
         try
@@ -133,21 +101,20 @@ public:
         }
         catch (CryptoPP::Exception& exception)
         {
-            Base::Logger::FileLogger::getInstance().log(exception.what(), Base::Logger::LogLevel::ERR);
-            return Utility::GeneralCodes::FAILED;
+            FileLogger::getInstance().log(exception.what(), LogLevel::ERR);
+            return EncryptionState::FAILURE;
         }
         if (!authDecrFilter.GetLastResult())
         {
-            Base::Logger::FileLogger::getInstance().log("Data integrity of message is violated", Base::Logger::LogLevel::ERR);
-            return Utility::GeneralCodes::FAILED;
-
+            FileLogger::getInstance().log("Data integrity of message is violated", LogLevel::ERR);
+            return EncryptionState::FAILURE;
         }
         auto               countBytesForReading = authDecrFilter.MaxRetrievable();
         yas::shared_buffer decryptedBody(countBytesForReading);
         authDecrFilter.Get(reinterpret_cast<byte*>(decryptedBody.data.get()), countBytesForReading);
 
         buffer = decryptedBody;
-        return Utility::GeneralCodes::SUCCESS;
+        return EncryptionState::SUCCESS;
     }
 
 private:
@@ -156,7 +123,5 @@ private:
     * because these are truncated 128-bit (16-bytes) tags.
     */
     static const uint8_t TAG_SIZE = 16;
-    std::string          _authData;  // hash of username
-    std::uint64_t        _userID;
 };
 }  // namespace Base::Crypto::Symmetric
--- Base/Crypto/Crypto.Static/ICryptography.hpp
+++ Base/Crypto/Crypto.Static/ICryptography.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "Network/YasSerializer.hpp"
-#include "Utility/Utility.hpp"
 
 namespace Base::Crypto
 {
+    using Network::EncryptionState;
 /** @class ICryptography
  * @brief Interface for encryption schemes
  * @details The intreface provides access to encryption and decryption for EncryptionHandler class.
@@ -15,10 +15,29 @@ namespace Base::Crypto
 class ICryptography
 {
 public:
-    /// @brief Method for encryption. Gets params from EncryptionHandler::handleOutcomingMessage
-    virtual Utility::GeneralCodes encrypt(const std::string& initVector, yas::shared_buffer& bodyBuffer) = 0;
-    /// @brief Method for decryption. Gets params from EncryptionHandler::handleIncomingMessage
-    virtual Utility::GeneralCodes decrypt(yas::shared_buffer& buffer, const std::string& initVector) = 0;
-    virtual ~ICryptography()                                                            = default;
+    /** @brief Method for encryption
+    * @param buffer[in,out] Reference to serialized and compressed data in buffer. After encryption will be substituted on
+    * encrypted data.
+    * @param key[in] Encryption key
+    * @param initVector[in] Initialization vector (may be unused in some implementation of encryption)
+    * @param authData[in] Authentification data, used for data authentification (may be unused)
+    */
+    virtual EncryptionState encrypt(yas::shared_buffer& buffer,
+                                    const SecByteBlock& key,
+                                    const SecByteBlock& initVector = SecByteBlock(0),
+                                    const std::string& authData = "") = 0;
+
+    /** @brief Method for decryption
+    * @param buffer[in,out] Reference to serialized, compressed and encrypted data in buffer
+    * @param key[in] Encryption key
+    * @param initVector[in] Initialization vector (may be unused in some implementation of encryption)
+    * @param authData[in] Authentification data, used for data authentification (may be unused)
+    */
+    virtual EncryptionState decrypt(yas::shared_buffer& buffer,
+                                    const SecByteBlock& key,
+                                    const SecByteBlock& initVector = SecByteBlock(0),
+                                    const std::string& authData = "") = 0;
+
+    virtual ~ICryptography() = default;
 };
 }  // namespace Base::Crypto
