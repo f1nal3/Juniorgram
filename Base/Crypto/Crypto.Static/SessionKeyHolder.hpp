700ab98291b616a5cd7338f986922ec274f1050d
Viachaslau Lepeshau <slavex001@tut.by>
2023-05-07 11:51:19 +0300

    Refactor of SessionKeyHolder

--- Base/Crypto/Crypto.Static/SessionKeyHolder.hpp
+++ Base/Crypto/Crypto.Static/SessionKeyHolder.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <any>
 #include <map>
 #include <cryptopp/secblock.h>
 
@@ -12,15 +11,6 @@ namespace Base
 using CryptoPP::SecByteBlock;
 using Pair_UserKey = std::pair<uint64_t, SecByteBlock>;
 using Map_UserKey  = std::map<uint64_t, SecByteBlock>;
-
- /** @enum Owner
- * @brief Determines content in SessionKeyHolder class
- */
-enum class Owner
-{
-    SERVER,
-    CLIENT
-};
 
  /** @class SessionKeyHolder
  * @brief Class contains session key on client-side or map of users session key on server-side
@@ -40,33 +30,40 @@ public:
         static SessionKeyHolder singleton;
         return singleton;
     }
-    /** @brief Method of setting current side
-     * @detail The method determines the functionality that will work
-     * @warning On client-side you must call this method to determine side
-     */
-    void setOwnerType(Owner owner_) { _owner = owner_; };
 
-    /** @brief Client's method of setting key
-     * @detail This method is used to set the key when connecting or updating the key
-     */
-    void setKey(SecByteBlock newKey) { _keyHolder = std::make_any<SecByteBlock>(newKey); };
-
-    /// @brief Client's method for getting key
-    SecByteBlock getKey() const { return std::any_cast<SecByteBlock>(_keyHolder); };
-
-    /// @brief Server's method to set client key on connection
-    void addUserKey(SecByteBlock newKey, uint64_t userId)
-    {
-        std::any_cast<Map_UserKey>(&_keyHolder)->insert(Pair_UserKey(userId, newKey));
-    };
-    /** @brief Server's method for refreshing user's key
-    * @details Method finds already connected user and change the key without the need to add a new user-key pair \
-    * This is draft method. It requires reaction to unsuccessful key update.
+    /* @brief Method to set key
+    * @param newKey key is result of key agreement protocol
+    * @param userId id, which is contained in Connection class
     */
-    Utility::GeneralCodes refreshUserKey(SecByteBlock newKey, const uint64_t& userId)
+    void setKey(SecByteBlock newKey, uint64_t userId) { _keysContainer.insert(Pair_UserKey(userId, newKey)); }
+
+    /// @brief Method of getting session key by user ID
+    SecByteBlock getKey(const uint64_t& userId) const
     {
-        if (auto findedUser = std::any_cast<Map_UserKey>(&_keyHolder)->find(userId);
-            findedUser != std::any_cast<Map_UserKey>(&_keyHolder)->end())
+        if (auto findedUser = _keysContainer.find(userId);
+            findedUser != _keysContainer.end())
+        {
+            return findedUser->second;
+        }
+        else
+        {
+            Base::Logger::FileLogger::getInstance().log(
+                std::string("Cannot get session key for userId = ") + std::to_string(userId),
+                Base::Logger::LogLevel::ERR);
+        }
+        return SecByteBlock(0);
+    }
+
+    /// @brief Method of removing session key by user ID
+    void removeKey(const uint64_t& userId) { _keysContainer.erase(userId); }
+
+    /** @brief Method for refreshing user's key
+    * @details Method finds already connected user and change the key without the need to add a new user-key pair.
+    */
+    Utility::GeneralCodes refreshKey(SecByteBlock newKey, uint64_t userId)
+    {
+        if (auto findedUser = _keysContainer.find(userId);
+            findedUser !=_keysContainer.end())
         {
             findedUser->second = newKey;
             return Utility::GeneralCodes::SUCCESS;
@@ -78,23 +75,10 @@ public:
                 Base::Logger::LogLevel::ERR);
         }
         return Utility::GeneralCodes::FAILED;
-    };
-    /// @brief Server's method of removing user session key by user ID
-    void removeUserKey(const uint64_t& userId) { std::any_cast<Map_UserKey>(&_keyHolder)->erase(userId); };
-
-    SecByteBlock getUserKey(const uint64_t& userId) { return std::any_cast<Map_UserKey>(_keyHolder).find(userId)->second; };
-
-    Owner getOwner() { return _owner; };
+    }
 
 private:
-    /** @brief _keyHolder contains session key(s)
-    * @detail Depending on the party _keyHolder can contain:
-    * - map<User,Key> on server-side
-    * - Key on client-side
-    */
-    std::any _keyHolder;
-    /// @brief The owner determines which functionality will be available and what the _keyHolder will contain
-    Owner _owner                              = Owner::SERVER;
+    Map_UserKey _keysContainer;
     SessionKeyHolder()                        = default;
     ~SessionKeyHolder()                       = default;
     SessionKeyHolder(const SessionKeyHolder&) = delete;
